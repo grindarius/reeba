598e7df9559d7a6b328cbe9157dd5237b1b42fff
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-12-10 17:35:07 +0700

    Test importing from `common`

--- packages/frontend/src/composables/use-counter.ts
+++ packages/frontend/src/composables/use-counter.ts
@@ -1,9 +1,12 @@
-import { Ref, ref } from 'vue'
+import { computed, ComputedRef, Ref, ref } from 'vue'
+
+import { meaningOfLife } from '@reeba/common'
 
 interface UseCounter {
   count: Ref<number>
   increment: () => void
   decrement: () => void
+  life: ComputedRef<number>
 }
 
 export const useCounter = (): UseCounter => {
@@ -17,9 +20,14 @@ export const useCounter = (): UseCounter => {
     count.value -= 1
   }
 
+  const life = computed(() => {
+    return meaningOfLife
+  })
+
   return {
     count,
     increment,
-    decrement
+    decrement,
+    life
   }
 }
