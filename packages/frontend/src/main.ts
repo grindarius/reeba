f4ad52ddf2c30182aa4c7a77977e05509c7c3cd7
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-12-11 00:46:41 +0700

    install `vue-meta`

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
+import { createMetaManager } from 'vue-meta'
 
 import App from './App.vue'
 import Router from './router'
 
 import './globals.scss'
 
-createApp(App).use(Router).mount('#app')
+createApp(App).use(Router).use(createMetaManager()).mount('#app')
