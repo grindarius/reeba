6839d97a221032448a372c7a38a609b2c6fa6a47
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-12-10 17:36:17 +0700

    convert typescript function to anonymous function

--- packages/backend/src/routes/index.ts
+++ packages/backend/src/routes/index.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify'
 
-export default async function (instance: FastifyInstance, _: FastifyPluginOptions): Promise<void> {
+export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promise<void> => {
   instance.get('/', async () => {
     return {
       author: 'Bhattarapong Somwong',
