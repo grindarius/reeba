ffa0fee1e9459ff43961959bde9a1ee48c017f9d
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-12-22 20:50:40 +0700

    Example of mocking data

---
+++ packages/frontend/src/constants/home.ts
@@ -0,0 +1,13 @@
+interface HomePageDetails {
+  imagePath: string
+  title: string
+  subtitle: string
+}
+
+export const homePageDetails: Array<HomePageDetails> = [
+  {
+    imagePath: '@/assets/...',
+    title: 'sdfdf',
+    subtitle: 'sdfdfd'
+  }
+]
