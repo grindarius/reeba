69a9048bc146c098fe346fb441fc6e2c38ff1d78
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-12-24 01:42:50 +0700

    Update database.sql

--- packages/backend/database.sql
+++ packages/backend/database.sql
@@ -1,9 +1,9 @@
 -- CREATE DATABASE IF NOT EXISTS reeba
 
 CREATE TABLE IF NOT EXISTS users(
-  user_id VARCHAR(100) NOT NULL,
-  user_name VARCHAR(15) NOT NULL,
-  user_email VARCHAR(255) NOT NULL UNIQUE,
-  user_password VARCHAR(255) NOT NULL,
-  PRIMARY KEY(user_id)
+  user_id TEXT NOT NULL,
+  user_name TEXT NOT NULL UNIQUE,
+  user_email TEXT NOT NULL UNIQUE,
+  user_password TEXT NOT NULL,
+  PRIMARY KEY (user_id)
 );
