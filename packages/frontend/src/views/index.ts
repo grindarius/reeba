a2ca1ea8ee0ea1b1055f30942284d0ac69053ab7
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-12-23 22:32:51 +0700

    Create page templates

--- packages/frontend/src/views/index.ts
+++ packages/frontend/src/views/index.ts
@@ -1,5 +1,19 @@
+import Account from './account.vue'
+import CreateEvent from './create-event.vue'
+import Event from './event.vue'
 import Home from './home.vue'
+import Login from './login.vue'
+import Payment from './payment.vue'
+import Receipt from './receipt.vue'
+import SelectSeat from './select-seat.vue'
 
 export {
-  Home
+  Account,
+  CreateEvent,
+  Event,
+  Home,
+  Login,
+  Payment,
+  Receipt,
+  SelectSeat
 }
