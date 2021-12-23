6aa8091817c2b4df1267761180a7c2d0edd694c6
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-12-23 18:38:38 +0700

    Rewrite hooks in waterfall style

--- packages/backend/src/routes/auth/index.ts
+++ packages/backend/src/routes/auth/index.ts
@@ -11,19 +11,27 @@ const registerSchema: FastifySchema = {
 }
 
 export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promise<void> => {
-  instance.post<{ Body: LoginParams }>('/login', { schema: loginSchema }, async (request) => {
-    const { email, password } = request.body
+  instance.post<{ Body: RegisterParams }>(
+    '/register',
+    {
+      schema: registerSchema
+    }, async (request) => {
+      const { username, email, password } = request.body
 
-    return {
-      message: `you have been to the login route, email is ${email}, password is ${password}`
-    }
-  })
+      return {
+        message: `you have been to the register route, username is ${username}, email is ${email}, password is ${password}`
+      }
+    })
 
-  instance.post<{ Body: RegisterParams }>('/register', { schema: registerSchema }, async (request) => {
-    const { username, email, password } = request.body
+  instance.post<{ Body: LoginParams }>(
+    '/login',
+    {
+      schema: loginSchema
+    }, async (request) => {
+      const { email, password } = request.body
 
-    return {
-      message: `you have been to the register route, username is ${username}, email is ${email}, password is ${password}`
-    }
-  })
+      return {
+        message: `you have been to the login route, email is ${email}, password is ${password}`
+      }
+    })
 }
