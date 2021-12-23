19719c317a219963231e16b879b6dde25c4ae15e
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-12-23 17:13:52 +0700

    Set up `fastify-postgres`, `fastify-jwt` and environment variables

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,26 +1,54 @@
+import dotenv from 'dotenv'
 import fastify, { FastifyInstance } from 'fastify'
 import cors from 'fastify-cors'
 import helmet from 'fastify-helmet'
+import jwt from 'fastify-jwt'
+import pg from 'fastify-postgres'
 import { exit } from 'process'
 
 import routes from './routes'
 
-const PORT = 3000
+dotenv.config()
+
+const PORT = process.env.FASTIFY_PORT ?? 3000
 
 const createServer = (): FastifyInstance => {
   const server = fastify({
     logger: true
   })
 
+  const pgUsername = process.env.POSTGRES_USERNAME
+  const pgPassword = process.env.POSTGRES_PASSWORD
+  const pgHostname = process.env.POSTGRES_HOSTNAME
+  const pgPort = process.env.POSTGRES_PORT
+  const pgDBName = process.env.POSTGRES_DBNAME
+  const jwtSecret = process.env.JWT_SECRET
+
+  if (
+    pgUsername == null ||
+    pgPassword == null ||
+    pgHostname == null ||
+    pgPort == null ||
+    pgDBName == null ||
+    jwtSecret == null
+  ) {
+    throw new Error('Missing one of postgres related credentials, or jwt secret')
+  }
+
   void server.register(cors)
   void server.register(helmet, { enableCSPNonces: true })
-
   void server.register(routes)
+  void server.register(pg, {
+    connectionString: `postgres://${pgUsername}:${pgPassword}@${pgHostname}:${pgPort}/${pgDBName}`
+  })
+  void server.register(jwt, {
+    secret: jwtSecret
+  })
 
   return server
 }
 
-createServer().listen(PORT).then((address) => {
+createServer().listen(Number(PORT)).then((address) => {
   console.log(`Server is running at ${address}`)
 }).catch((e) => {
   console.trace(e)
