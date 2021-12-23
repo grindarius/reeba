b48de27ebd6ad6fbd10428e0fa029d02a8f5881b
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-12-24 01:42:01 +0700

    Add docs to `validateEmail` function

--- packages/common/src/utils/validate-email.ts
+++ packages/common/src/utils/validate-email.ts
@@ -1,5 +1,14 @@
 const emailRegExp: RegExp = /^([\w-.]+)@([\w]+)\.([\w]+)$/
 
+/**
+ * Function for validating user emails wheter it's the right format or not.
+ *
+ * The format starts with word, dot and dash starts with length of 1, followed by `@` with
+ * any word, followed by `.` and a word at last part.
+ *
+ * @param email email string
+ * @returns boolean states that whether the email is the correct format or not.
+ */
 export const validateEmail = (email: string): boolean => {
   return emailRegExp.test(email)
 }
