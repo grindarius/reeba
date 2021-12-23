c42e6fd10c71b861394e22d1d4ea5cd804c539db
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-12-24 01:42:15 +0700

    Update `/register` route

--- packages/backend/src/routes/auth/index.ts
+++ packages/backend/src/routes/auth/index.ts
@@ -1,10 +1,13 @@
+import bcrypt from 'bcrypt'
 import { FastifyInstance, FastifyPluginOptions, FastifySchema } from 'fastify'
+import { nanoid } from 'nanoid'
 
 import {
   LoginParams,
   LoginParamsSchema,
   RegisterParams,
-  RegisterParamsSchema
+  RegisterParamsSchema,
+  RegisterReplySchema
 } from '@reeba/common'
 
 const loginSchema: FastifySchema = {
@@ -12,25 +15,56 @@ const loginSchema: FastifySchema = {
 }
 
 const registerSchema: FastifySchema = {
-  body: RegisterParamsSchema
+  body: RegisterParamsSchema,
+  response: {
+    200: RegisterReplySchema
+  }
 }
 
 export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promise<void> => {
   instance.post<{ Body: RegisterParams }>(
     '/register',
     {
-      schema: registerSchema,
-      preValidation: (request, _, done) => {
-        console.log(request.body)
-        done()
-      }
+      schema: registerSchema
     }, async (request) => {
       const { username, email, password } = request.body
 
-      return {
-        message: `you have been to the register route, username is ${username}, email is ${email}, password is ${password}`
+      try {
+        const possibleDuplicateEmails = await instance.pg.query<{ user_email: string }, [string]>(
+          'SELECT * FROM users WHERE user_email = $1',
+          [email]
+        )
+
+        if (possibleDuplicateEmails.rows.length > 0) {
+          throw new Error('Duplicate email')
+        }
+      } catch (error) {
+        throw new Error(`Error while finding email duplicates ${error as string}`)
       }
-    })
+
+      const userId = nanoid(25)
+      const salt = await bcrypt.genSalt(10)
+      const encryptedPassword = await bcrypt.hash(password, salt)
+
+      await instance.pg.query<{
+        user_id: string
+        user_name: string
+        user_email: string
+        user_password: string
+      }, [string, string, string, string]>(
+        'INSERT INTO users (user_id, user_name, user_email, user_password) VALUES ($1, $2, $3, $4)',
+        [userId, username, email, encryptedPassword]
+      )
+
+      const token = instance.jwt.sign({ user: { id: userId } }, {
+        expiresIn: '5m'
+      })
+
+      return {
+        token
+      }
+    }
+  )
 
   instance.post<{ Body: LoginParams }>(
     '/login',
@@ -42,5 +76,6 @@ export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promi
       return {
         message: `you have been to the login route, email is ${email}, password is ${password}`
       }
-    })
+    }
+  )
 }
