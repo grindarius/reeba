726328b6cfb8dba1fa2edf7a4fc843f03a4db763
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-12-24 23:18:33 +0700

    Add definitions for database typescript type inference

--- packages/backend/src/types/database.ts
+++ packages/backend/src/types/database.ts
@@ -1,6 +1,21 @@
+/**
+ * Table that stores user informations.
+ */
 export interface users {
+  /**
+   * id of each user. Unique. Not null. generated using `nanoid(25)`. Primary key.
+   */
   user_id: string
+  /**
+   * User's username, will not exceed 15 characters. Not null.
+   */
   user_name: string
+  /**
+   * User's email. Not null. Unique.
+   */
   user_email: string
+  /**
+   * User's password. Not null, cannot be longer than 64 characters but no length limit in database.
+   */
   user_password: string
 }
