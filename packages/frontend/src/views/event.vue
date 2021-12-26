b7511bc354fea16388b6e2bf7b6416a2ca38246d
PawinaK <95273691+PawinaK@users.noreply.github.com>
2021-12-26 15:16:21 +0700

    bts text

--- packages/frontend/src/views/event.vue
+++ packages/frontend/src/views/event.vue
@@ -1,8 +1,9 @@
 <template>
 <div class="event-page">
-  <div class="event-page-content">
-    <img class="object-none object-left-top " src="@/assets/bts-world-tour.jpg">
-  </div>
+ <div class="event-page-content">
+   <img class="object-none object-left-top float-left text-5xl " src="@/assets/bts-world-tour.jpg">
+   <p class="indent-8 text-4xl text-white ml-100 mt-10">BTS WORLD TOUR 'LOVE YOURSELF' BANGKOK</p>
+ </div>
 </div>
 </template>
 
