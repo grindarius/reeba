824b16c89f3e79c69dcbc8d7268a1b995c3e3e59
PhonratM <phonratm62@nu.ac.th>
2021-12-29 02:12:52 +0700

    เพิ่ม effect
    เพิ่ม effect เงาให้กล่อง username และ password เปลี่ยนรูปร่าง

--- packages/frontend/src/views/login.vue
+++ packages/frontend/src/views/login.vue
@@ -1,68 +1,37 @@
 <template>
 <div class="login-page">
-  <div class="w-full h-screen flex items-center justify-center">
-  <form class="w-full md:w-1/3 rounded-lg  ">
-  <div class="flex font-bold justify-center scroll-mt-50">
-   <img class="h-64 w-64 mb-3" src="@/assets/reeba-logo.png" alt="logo-reeba" />
-  </div>
-
-  <div class="px-12 pb-10">
-     <div class="w-full mb-2">
-    <div class="flex items-center">
-     <input
-			type="text"
-			placeholder="Email Address"
-			class="
-			 	w-full
-			  border
-			  rounded
-			  px-3
-			  py-2
-			  text-gray-700
-			  focus:outline-none "
-		  />
-		</div>
-	  </div>
-	  <div class="w-full mb-2">
-		<div class="flex items-center">
-		  <input
-			type="password"
-			placeholder="Password"
-			class="
-			  w-full
-			  border
-			  rounded
-			  px-3
-			  py-2
-			  text-gray-700
-			  focus:outline-none
-			"
-		  />
-		</div>
-	  </div>
-	  <button
-		type="submit"
-		class="
-		  w-full
-		  py-2
-		  mt-8
-		  rounded-full
-		  bg-pale-yellow
-		  text-gray-100
-		  focus:outline-none
-		"
-	  >
-		Login
-	  </button><br>
-    <a class="inline-block align-baseline font-bold text-sm text-white hover:text-blue-darker" href="#">
+<div class="w-full h-screen flex items-center justify-center">
+<form class="w-full md:w-1/3 rounded-lg  ">
+<div class="bg-neutral-700 shadow-xl rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+	<div class="flex font-bold justify-center scroll-mt-50">
+		<img class="h-64 w-64 mb-3" src="@/assets/reeba-logo.png" alt="logo-reeba">
+	</div>
+	<div class="mb-4">
+      <label class="block text-white text-sm font-sans mb-2" for="username">
+        Username
+      </label>
+      <input class=" shadow-lg shadow-slate-900 appearance-none border rounded-full w-full py-2 px-3 text-grey-darker" id="username" type="text" placeholder="Username">
+    </div>
+	 <div class="mb-6">
+      <label class="block text-white text-sm font-sans mb-2" for="password">
+        Password
+      </label>
+      <input class="shadow-lg shadow-slate-900  appearance-none border border-red rounded-full w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="password">
+    </div>
+		<div class="flex items-center justify-between">
+      <button class="bg-pale-yellow text-gray font-sans py-2 px-8 rounded-full" type="button">
+        Sign In
+      </button>
+      <a class="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker" href="#">
         Forgot Password?
-    </a>
-  </div>
-  </form>
+      </a>
+    </div>
+</div>
+</form>
 </div>
 </div>
-
 </template>
+
 <script lang="ts">
 import { defineComponent } from 'vue'
 
