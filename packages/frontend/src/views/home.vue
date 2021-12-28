ec80d54434890ddc4fede489c394043577173b53
LzzAa35 <62424250+LzzAa35@users.noreply.github.com>
2021-12-28 23:34:46 +0700

    fix image, add shadow, import info

--- packages/frontend/src/views/home.vue
+++ packages/frontend/src/views/home.vue
@@ -2,90 +2,82 @@
 <div class="home-page">
   <div class="w-full min-h-screen container">
     <!-- Start first box here... -->
-    <div>
-      <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 class="text-4xl font-bold font-sans text-white">
+    <div class="Official-events-box">
+      <div class="whole-event-box">
+        <h2 class="text-Main-event-name">
           Official events
         </h2>
-        <!-- Cofficial Envent start here... -->
+        <!-- Official Envent start here... -->
         <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          <div class="group relative">
-            <div class="w-full bg-gray-200 rounded-t-lg overflow-hidden group-hover:opacity-75">
-              <img src="@/assets/TK-1-Lido.png" alt="Front of men&#039;s Basic Tee in black." class="w-full h-full object-center object-cover lg:w-full lg:h-full">
+          <div class="whole-ticket">
+            <div class="image-ticket-box">
+              <img class="image-ticket-main-image" src="@/assets/TK-1-Lido.png" alt="Front of men&#039;s Basic Tee in black.">
             </div>
             <div class="ticket-text-info">
               <div>
-                <h3 class="text-sm text-black">
-                  <a href="#">
-                    <span aria-hidden="true" class="absolute inset-0"></span>
-                    Basic Tee
-                  </a>
+                <h3 class="ticket-text-name">
+                    LIDO CONNECT - Movie Program
                 </h3>
-                <p class="mt-1 text-sm text-white">Black</p>
+                <p class="ticket-text-time">16 JAN 2022 | 13:00 PM</p>
+                <p class="ticket-text-location">LIDO CONNECT Hall 1</p>
               </div>
-              <p class="text-sm font-medium text-gray-900">$35</p>
+              <!--p-- class="text-sm font-medium text-gray-900">$35</!--p-->
             </div>
           </div>
-          <div class="group relative">
-            <div class="w-full bg-gray-200 rounded-t-lg overflow-hidden group-hover:opacity-75">
-              <img src="@/assets/TK-2-BTS.png" alt="Front of men&#039;s Basic Tee in black." class="w-full h-full object-center object-cover lg:w-full lg:h-full">
+          <div class="whole-ticket">
+            <div class="image-ticket-box">
+              <img class="image-ticket-main-image" src="@/assets/TK-2-BTS.png" alt="Front of men&#039;s Basic Tee in black.">
             </div>
             <div class="ticket-text-info">
               <div>
-                <h3 class="text-sm text-black">
-                <a href="#">
-                  <span aria-hidden="true" class="absolute inset-0"></span>
-                  Basic Tee
-                </a>
+                <h3 class="ticket-text-name">
+                  BTS World Tour 'Love Yourself' Bangkok
                 </h3>
-                <p class="mt-1 text-sm text-white">Black</p>
+                <p class="ticket-text-time">6 - 7 Apr 2021 | 13:00 PM</p>
+                <p class="ticket-text-location">Rajamangkala National</p>
               </div>
-              <p class="text-sm font-medium text-gray-900">$35</p>
+              <!--p-- class="text-sm font-medium text-gray-900">$35</!--p-->
             </div>
           </div>
-          <div class="group relative">
-            <div class="w-full bg-gray-200 rounded-t-lg overflow-hidden group-hover:opacity-75">
-              <img src="@/assets/TK-3-ReMSC.png" alt="Front of men&#039;s Basic Tee in black." class="w-full h-full object-center object-cover lg:w-full lg:h-full">
+          <div class="whole-ticket">
+            <div class="image-ticket-box">
+              <img class="image-ticket-main-image" src="@/assets/TK-3-ReMSC.png" alt="Front of men&#039;s Basic Tee in black.">
             </div>
             <div class="ticket-text-info">
               <div>
-                <h3 class="text-sm text-black">
-                <a href="#">
-                    <span aria-hidden="true" class="absolute inset-0"></span>
-                    Basic Tee
-                </a>
+                <h3 class="ticket-text-name">
+                    Re-MSC: Reconnecting with Mindful Self-Compassion
                 </h3>
-                <p class="mt-1 text-sm text-white">Black</p>
+                <p class="ticket-text-time">25 APR 2022 | 13:00 PM</p>
+                <p class="ticket-text-location">Online Event</p>
               </div>
-              <p class="text-sm font-medium text-gray-900">$35</p>
+              <!--p-- class="text-sm font-medium text-gray-900">$35</!--p-->
             </div>
           </div>
-          <div class="group relative">
-            <div class="w-full bg-gray-200 rounded-t-lg overflow-hidden group-hover:opacity-75">
-              <img src="@/assets/TK-4-Sculpture Drive Thru.png" alt="Front of men&#039;s Basic Tee in black." class="w-full h-full object-center object-cover lg:w-full lg:h-full">
+          <div class="whole-ticket">
+            <div class="image-ticket-box">
+              <img class="image-ticket-main-image" src="@/assets/TK-4-Sculpture Drive Thru.png" alt="Front of men&#039;s Basic Tee in black.">
             </div>
             <div class="ticket-text-info">
               <div>
-                <h3 class="text-sm text-black">
-                <a href="#">
-                  <span aria-hidden="true" class="absolute inset-0"></span>
-                  Basic Tee
-                </a>
+                <h3 class="ticket-text-name">
+                  Sculpture Drive Thru
                 </h3>
-                <p class="mt-1 text-sm text-white">Black</p>
+                <p class="ticket-text-time">5 MAR 2022 | 13:00 PM</p>
+                <p class="ticket-text-location">Day Day Ari, Ari Samphan Soi 5</p>
               </div>
-              <p class="text-sm font-medium text-gray-900">$35</p>
+              <!--p-- class="text-sm font-medium text-gray-900">$35</!--p-->
             </div>
           </div>
-        <!-- Cofficial Envent end here... -->
+        <!-- Official Envent end here... -->
         </div>
       </div>
       <!-- End of first box... -->
     </div>
       <!-- Start seconde box here... -->
-      <div>
-      <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 class="text-4xl font-bold font-sans text-white">
+    <div class="Local-events-box">
+      <div class="whole-event-box">
+        <h2 class="text-Main-event-name">
           Local events
         </h2>
 
@@ -93,75 +85,67 @@
         <div class="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
 
           <!-- Cofficial Envent start here... -->
-          <div class="group relative">
-            <div class="w-full bg-gray-200 rounded-t-lg overflow-hidden group-hover:opacity-75">
-              <img src="@/assets/TK-5-PGBLive.png" alt="Front of men&#039;s Basic Tee in black." class="w-full h-full object-center object-cover lg:w-full lg:h-full">
+          <div class="whole-ticket">
+            <div class="image-ticket-box">
+              <img class="image-ticket-main-image" src="@/assets/TK-5-PGBLive.png" alt="Front of men&#039;s Basic Tee in black.">
             </div>
             <div class="ticket-text-info">
               <div>
-                <h3 class="text-sm text-black">
-                <a href="#">
-                    <span aria-hidden="true" class="absolute inset-0"></span>
-                    Basic Tee
-                </a>
+                <h3 class="ticket-text-name">
+                    PGB Live: Next Level Pinoy
                 </h3>
-                <p class="mt-1 text-sm text-white">Black</p>
+                <p class="ticket-text-time">10 MAR 2022 | 13:00 PM</p>
+                <p class="ticket-text-location">Virtual Event</p>
               </div>
-              <p class="text-sm font-medium text-gray-900">$35</p>
+              <!--p-- class="text-sm font-medium text-gray-900">$35</!--p-->
             </div>
           </div>
 
-                  <div class="group relative">
-            <div class="w-full bg-gray-200 rounded-t-lg overflow-hidden group-hover:opacity-75">
-              <img src="@/assets/TK-6-Child in Mob.png" alt="Front of men&#039;s Basic Tee in black." class="w-full h-full object-center object-cover lg:w-full lg:h-full">
+          <div class="whole-ticket">
+            <div class="image-ticket-box">
+              <img class="image-ticket-main-image" src="@/assets/TK-6-Child in Mob.png" alt="Front of men&#039;s Basic Tee in black.">
             </div>
             <div class="ticket-text-info">
               <div>
-                <h3 class="text-sm text-black">
-                <a href="#">
-                    <span aria-hidden="true" class="absolute inset-0"></span>
-                    Basic Tee
-                </a>
+                <h3 class="ticket-text-name">
+                    Child in Mob
                 </h3>
-                <p class="mt-1 text-sm text-white">Black</p>
+                <p class="ticket-text-time">9 JUN 2022 | 13:00 PM</p>
+                <p class="ticket-text-location">Charity</p>
               </div>
-              <p class="text-sm font-medium text-gray-900">$35</p>
+              <!--p-- class="text-sm font-medium text-gray-900">$35</!--p-->
             </div>
           </div>
 
-                  <div class="group relative">
-            <div class="w-full bg-gray-200 rounded-t-lg overflow-hidden group-hover:opacity-75">
-              <img src="@/assets/TK-7-Transport  Chang Chui 2.png" alt="Front of men&#039;s Basic Tee in black." class="w-full h-full object-center object-cover lg:w-full lg:h-full">
+          <div class="whole-ticket">
+            <div class="image-ticket-box">
+              <img class="image-ticket-main-image" src="@/assets/TK-7-Transport  Chang Chui 2.png" alt="Front of men&#039;s Basic Tee in black.">
             </div>
             <div class="ticket-text-info">
               <div>
-                <h3 class="text-sm text-black">
-                <a href="#">
-                    <span aria-hidden="true" class="absolute inset-0"></span>
-                    Basic Tee
-                </a>
+                <h3 class="ticket-text-name">
+                    Transport | Chang Chui 2
                 </h3>
-                <p class="mt-1 text-sm text-white">Black</p>
+                <p class="ticket-text-time">19 JAN 2022 | 13:00 PM</p>
+                <p class="ticket-text-location">Chang Chui</p>
               </div>
-              <p class="text-sm font-medium text-gray-900">$35</p>
+              <!--p-- class="text-sm font-medium text-gray-900">$35</!--p-->
             </div>
           </div>
 
-                  <div class="group relative">
-            <div class="w-full bg-gray-200 rounded-t-lg overflow-hidden group-hover:opacity-75">
-              <img src="@/assets/TK-8-Hg80 Bar Vouchers.png" alt="Front of men&#039;s Basic Tee in black." class="w-full h-full object-center object-cover lg:w-full lg:h-full">
+          <div class="whole-ticket">
+            <div class="image-ticket-box">
+              <img class="image-ticket-main-image" src="@/assets/TK-8-Hg80 Bar Vouchers.png" alt="Front of men&#039;s Basic Tee in black.">
             </div>
             <div class="ticket-text-info">
               <div>
-                <h3 class="text-sm text-black">
-                <a href="#">
-                    <span aria-hidden="true" class="absolute inset-0"></span>
-                    Basic Tee
-                </a>
+                <h3 class="ticket-text-name">
+                    Hg80 Bar Vouchers
                 </h3>
-                <p class="mt-1 text-sm text-white">Black</p>
+                <p class="ticket-text-time">1 DEC 2022 | 13:00 PM</p>
+                <p class="ticket-text-location">Hg80 Bar Bangkok (near Chong Nonsi BTS)</p>
               </div>
-              <p class="text-sm font-medium text-gray-900">$35</p>
+              <!--p-- class="text-sm font-medium text-gray-900">$35</!--p-->
             </div>
           </div>
         <!-- Local Envent end here... -->
@@ -194,9 +178,33 @@ export default defineComponent({
 .home-page {
   @apply bg-pale-gray flex flex-row justify-center;
 }
-
+.text-Main-event-name {
+  @apply text-4xl font-bold font-sans text-white;
+}
+.whole-event-box {
+  @apply max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8;
+}
+.whole-ticket {
+  /*ease-in duration-300*/
+  @apply rounded-lg shadow-xl cursor-pointer hover:scale-105;
+}
+.image-ticket-box {
+  @apply bg-gray-200 rounded-t-lg overflow-hidden group-hover:opacity-75;
+}
+.image-ticket-main-image {
+  @apply w-full h-full object-center object-cover lg:w-full lg:h-full;
+}
 .ticket-text-info {
   /*mt-4 flex justify-between bg-yellow-400*/
-  @apply basis-1/4 h-14 rounded-b-lg flex  justify-between bg-pale-yellow;
+  @apply flex flex-col indent-[3%] h-20 rounded-b-lg  justify-self-start bg-pale-yellow;
+}
+.ticket-text-name {
+  @apply truncate mt-0.5 text-sm font-bold text-black;
+}
+.ticket-text-time{
+  @apply truncate mt-1 text-sm font-normal text-white;
+}
+.ticket-text-location{
+  @apply truncate mt-1 text-sm font-normal text-white;
 }
 </style>
