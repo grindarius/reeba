d52cf0aef2f91a656c5866947c0eb5f33bd029c5
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-12-29 22:28:21 +0700

    Refactor receipt page.

--- packages/frontend/src/views/receipt.vue
+++ packages/frontend/src/views/receipt.vue
@@ -1,62 +1,91 @@
 <template>
-  <div class="flex justify-center bg-pale-gray w-full min-h-screen">
-    <div class="container">
-      <div class="flex justify-center px-32 py-10">
-        <div class="w-full h-full bg-zinc-600 rounded-md">
-          <div class="justify-center px-8 py-8">
-            <div><p class="text-right block font-bold text-[25px] text-white text-xl" for="receipt">Payment Receipt</p></div><div class="py-4"></div>
-            <div class="flex justify-between">
-              <div><p class="font-bold text-white text-xl leading-loose" for="event-name">Event Name</p></div>
-              <div class="grid grid-cols-[500px] rounded-lg bg-white text-base leading-loose"></div>
-            </div><div class="py-2"></div>
-            <div class="flex justify-between">
-              <div><p class="font-bold text-white text-xl leading-loose" for="venue">Venue</p></div>
-              <div class="grid grid-cols-[500px] rounded-lg bg-white text-base leading-loose"></div>
-            </div><div class="py-2"></div>
-            <div class="flex justify-between">
-              <div><p class="font-bold text-white text-xl leading-loose" for="date&time">Date & Time</p></div>
-              <div class="grid grid-cols-[500px] rounded-lg bg-white text-base leading-loose"></div>
-            </div><div class="py-2"></div>
-            <div class="flex justify-between">
-              <div><p class="font-bold text-white text-xl leading-loose" for="zone">Zone</p></div>
-              <div class="grid grid-cols-[500px] rounded-lg bg-white text-base leading-loose"></div>
-            </div><div class="py-2"></div>
-            <div class="flex justify-between">
-              <div><p class="font-bold text-white text-xl leading-loose" for="seat-number">Seat NO</p></div>
-              <div class="grid grid-cols-[500px] rounded-lg bg-white text-base leading-loose"></div>
-            </div><div class="py-2"></div>
-            <div class="flex justify-between">
-              <div><p class="font-bold text-white text-xl leading-loose" for="reserve-id">Reserve ID</p></div>
-              <div class="grid grid-cols-[500px] rounded-lg bg-white text-base leading-loose"></div>
-            </div><div class="py-2"></div>
-            <div class="flex justify-between">
-              <div><p class="font-bold text-white text-xl leading-loose" for="quantity">Quantity</p></div>
-              <div class="grid grid-cols-[500px] rounded-lg bg-white text-base leading-loose"></div>
-            </div><div class="py-2"></div>
-            <div class="flex justify-between">
-              <div><p class="font-bold text-white text-xl leading-loose" for="unit-price">Unit Price (Baht)</p></div>
-              <div class="grid grid-cols-[500px] rounded-lg bg-white text-base leading-loose"></div>
-            </div><div class="py-2"></div>
-            <div class="flex justify-between">
-              <div><p class="font-bold text-white text-xl leading-loose" for="discount">Discount</p></div>
-              <div class="grid grid-cols-[500px] rounded-lg bg-white text-base leading-loose"></div>
-            </div><div class="py-2"></div>
-            <div class="flex justify-between">
-              <div><p class="font-bold text-white text-xl leading-loose" for="service-fee">Service Fee (40 Baht per ticket)</p></div>
-              <div class="grid grid-cols-[500px] rounded-lg bg-white text-base leading-loose"></div>
-            </div><div class="py-2"></div>
-            <div class="flex justify-between">
-              <div><p class="font-bold text-white text-xl leading-loose" for="shipping">Shipping (Baht)</p></div>
-              <div class="grid grid-cols-[500px] rounded-lg bg-white text-base leading-loose"></div>
-            </div><div class="py-2"></div>
-            <div class="flex justify-between">
-              <div><p class="font-bold text-white text-xl leading-loose" for="credit-card-fee">Credit Card Fee (Baht)</p></div>
-              <div class="grid grid-cols-[500px] rounded-lg bg-white text-base leading-loose"></div>
-            </div><div class="py-2"></div>
-            <div class="flex justify-between">
-              <div><p class="font-bold text-white text-xl leading-loose" for="shipping-information">Shipping Information</p></div>
-              <div class="grid grid-cols-[500px] rounded-lg bg-white text-base leading-loose"></div>
-            </div>
+  <div class="receipt-page">
+    <div class="receipt-page-content">
+      <div class="receipt-section">
+        <div class="receipt-section-header">
+          <div class="flex flex-row justify-center items-center">
+            <h1 class="text-white text-4xl">Payment receipt</h1>
+          </div>
+          <img src="@/assets/sample-qr-code.png" alt="QR code">
+        </div>
+        <div class="receipt-section-description">
+          <h1 class="receipt-header">
+            Event name
+          </h1>
+          <div class="receipt-description-box">
+            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+          </div>
+          <h1 class="receipt-header">
+            Venue
+          </h1>
+          <div class="receipt-description-box">
+            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+          </div>
+          <h1 class="receipt-header">
+            Date & Time
+          </h1>
+          <div class="receipt-description-box">
+            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+          </div>
+          <h1 class="receipt-header">
+            Zone
+          </h1>
+          <div class="receipt-description-box">
+            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+          </div>
+          <h1 class="receipt-header">
+            Seat number
+          </h1>
+          <div class="receipt-description-box">
+            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+          </div>
+          <h1 class="receipt-header">
+            Reserve ID
+          </h1>
+          <div class="receipt-description-box">
+            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+          </div>
+          <h1 class="receipt-header">
+            Quantity
+          </h1>
+          <div class="receipt-description-box">
+            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+          </div>
+          <h1 class="receipt-header">
+            Unit price (THB)
+          </h1>
+          <div class="receipt-description-box">
+            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+          </div>
+          <h1 class="receipt-header">
+            Discount
+          </h1>
+          <div class="receipt-description-box">
+            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+          </div>
+          <h1 class="receipt-header">
+            Service fee (40 THB/ticket)
+          </h1>
+          <div class="receipt-description-box">
+            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+          </div>
+          <h1 class="receipt-header">
+            Shipping cost (THB)
+          </h1>
+          <div class="receipt-description-box">
+            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+          </div>
+          <h1 class="receipt-header">
+            Credit card fee (THB)
+          </h1>
+          <div class="receipt-description-box">
+            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+          </div>
+          <h1 class="receipt-header">
+            Shipping information
+          </h1>
+          <div class="receipt-description-box">
+            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
           </div>
         </div>
       </div>
@@ -71,3 +100,37 @@ export default defineComponent({
   name: 'receipt'
 })
 </script>
+
+<style lang="scss">
+.receipt-page {
+  @apply flex justify-center bg-pale-gray w-full min-h-screen;
+}
+
+.receipt-page-content {
+  @apply container my-10;
+}
+
+.receipt-section {
+  @apply w-full h-full bg-zinc-600 rounded-md p-8;
+}
+
+.receipt-section-header {
+  @apply flex flex-row justify-between mb-6;
+}
+
+.receipt-section-description {
+  @apply grid grid-flow-row grid-cols-3 gap-4;
+}
+
+.receipt-header {
+  @apply text-white text-2xl;
+}
+
+.receipt-description-box {
+  @apply bg-white leading-loose px-2 rounded-lg col-span-2;
+}
+
+.receipt-description-box-text {
+  @apply text-xl leading-loose;
+}
+</style>
