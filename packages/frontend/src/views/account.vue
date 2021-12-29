fe8cd9248f4b63a93ffcea90dc6de867e5f1391b
Baramee Chaiho <pahe.014700@gmail.com>
2021-12-29 17:30:18 +0700

    Add and Fix
    Add
    -Add content box
    Fix
    -Fix the name to make it meaningful
    -Edit lines 48-50 to be more concise.

--- packages/frontend/src/views/account.vue
+++ packages/frontend/src/views/account.vue
@@ -45,9 +45,24 @@
         </div>
       </nav>
       <div class="account-content">
-        <h2 class="text-3xl font-sans text-white m-8">
-          My tickets
-        </h2>
+        <h2 class="content-heading">My tickets</h2>
+        <section class="content-box-size">
+          <div class="content-box-inside">
+            <div class="content-box-setting-image">
+              <img  class="content-box-picture" src="src/assets/photo.jpg">
+            </div>
+            <div class="content-box-textbox">
+              <p class="content-box-frontsize-title">Rajamangala National Stadium</p>
+              <p class="content-box-frontsize-main">Seat</p>
+              <p class="content-box-frontsize-message">C4-C09</p>
+              <p class="content-box-frontsize-main">Prices</p>
+              <p class="content-box-frontsize-message">6800THB</p>
+              <p class="content-box-frontsize-main">Show date</p>
+              <p class="content-box-frontsize-message">Wednesday, 6 April 2022</p>
+              <p class="content-box-frontsize-message">Thursday, 7 April 2022</p>
+            </div>
+          </div>
+        </section>
       </div>
   </div>
 </div>
@@ -88,5 +103,35 @@ export default defineComponent({
 
 .account-content {
   @apply grow;
+}
+.content-box {
+ @apply w-full my-1 border-none
+}
+.content-box-frontsize-title {
+  @apply text-sm text-gray-900 font-bold
+}
+.content-box-frontsize-main {
+  @apply text-xs text-gray-900 font-normal
+}
+.content-box-frontsize-message {
+  @apply text-xs text-gray-600 font-normal
+}
+.content-box-size {
+  @apply max-w-md mx-10 px-4 sm:px-6 lg:px-2 py-4
+}
+.content-box-inside {
+  @apply w-full bg-pale-yellow rounded-lg  overflow-hidden flex flex-col md:flex-row
+}
+.content-box-picture {
+  @apply object-center object-cover h-full w-full
+}
+.content-box-setting-image {
+  @apply w-full md:w-2/5 h-60
+}
+.content-box-textbox {
+  @apply w-full md:w-3/5 text-left p-6 md:p-4 space-y-2
+}
+.content-heading {
+  @apply text-3xl font-bold text-white m-8
 }
 </style>
