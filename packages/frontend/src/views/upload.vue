e4c147636e03a363d396bcba4ece49b07c34f8bc
Bhattarapong Somwong <numbbutt34685@gmail.com>
2021-12-31 00:09:37 +0700

    Create upload.vue

---
+++ packages/frontend/src/views/upload.vue
@@ -0,0 +1,13 @@
+<template>
+<div class="upload-test">
+
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'upload'
+})
+</script>
