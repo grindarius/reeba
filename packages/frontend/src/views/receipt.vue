c01ca74e49ab3df34f04356c9c32433d58a33121
tontalo <nuntawath62@nu.ac.th>
2021-12-31 00:48:27 +0700

    Balance container and enter data in box

--- packages/frontend/src/views/receipt.vue
+++ packages/frontend/src/views/receipt.vue
@@ -3,8 +3,8 @@
     <div class="receipt-page-content">
       <div class="receipt-section">
         <div class="receipt-section-header">
-          <div class="flex flex-row justify-center items-center">
-            <h1 class="text-white text-4xl">Payment receipt</h1>
+          <div class="flex flex-row justify-center items-start">
+            <h1 class="py-8 text-white text-4xl font-bold">Payment receipt</h1>
           </div>
           <img src="@/assets/sample-qr-code.png" alt="QR code">
         </div>
@@ -13,79 +13,79 @@
             Event name
           </h1>
           <div class="receipt-description-box">
-            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+            <span class="receipt-description-box-text">BTS WORLD TOUR 'LOVE YOURSELF' BANGKOK</span>
           </div>
           <h1 class="receipt-header">
             Venue
           </h1>
           <div class="receipt-description-box">
-            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+            <span class="receipt-description-box-text">Rajamangkala National Stadium</span>
           </div>
           <h1 class="receipt-header">
             Date & Time
           </h1>
           <div class="receipt-description-box">
-            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+            <span class="receipt-description-box-text">6 April 2022 19:00</span>
           </div>
           <h1 class="receipt-header">
             Zone
           </h1>
           <div class="receipt-description-box">
-            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+            <span class="receipt-description-box-text">C4</span>
           </div>
           <h1 class="receipt-header">
             Seat number
           </h1>
           <div class="receipt-description-box">
-            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+            <span class="receipt-description-box-text">C09</span>
           </div>
           <h1 class="receipt-header">
             Reserve ID
           </h1>
           <div class="receipt-description-box">
-            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+            <span class="receipt-description-box-text">RB1234567890</span>
           </div>
           <h1 class="receipt-header">
             Quantity
           </h1>
           <div class="receipt-description-box">
-            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+            <span class="receipt-description-box-text">1</span>
           </div>
           <h1 class="receipt-header">
             Unit price (THB)
           </h1>
           <div class="receipt-description-box">
-            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+            <span class="receipt-description-box-text">6400</span>
           </div>
           <h1 class="receipt-header">
             Discount
           </h1>
           <div class="receipt-description-box">
-            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+            <span class="receipt-description-box-text">0</span>
           </div>
           <h1 class="receipt-header">
             Service fee (40 THB/ticket)
           </h1>
           <div class="receipt-description-box">
-            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+            <span class="receipt-description-box-text">40</span>
           </div>
           <h1 class="receipt-header">
             Shipping cost (THB)
           </h1>
           <div class="receipt-description-box">
-            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+            <span class="receipt-description-box-text">50</span>
           </div>
           <h1 class="receipt-header">
             Credit card fee (THB)
           </h1>
           <div class="receipt-description-box">
-            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+            <span class="receipt-description-box-text">5</span>
           </div>
           <h1 class="receipt-header">
             Shipping information
           </h1>
           <div class="receipt-description-box">
-            <span class="receipt-description-box-text">BTS 'LOVE YOUR SELF'</span>
+            <span class="receipt-description-box-text">123, Singhawat, Phitsanulok, 65000</span>
           </div>
         </div>
       </div>
@@ -103,11 +103,11 @@ export default defineComponent({
 
 <style lang="scss">
 .receipt-page {
-  @apply flex justify-center bg-pale-gray w-full min-h-screen;
+  @apply flex flex-row justify-center bg-pale-gray w-full min-h-screen;
 }
 
 .receipt-page-content {
-  @apply container my-10;
+  @apply container my-10 basis-3/5;
 }
 
 .receipt-section {
@@ -127,7 +127,7 @@ export default defineComponent({
 }
 
 .receipt-description-box {
-  @apply bg-white leading-loose px-2 rounded-lg col-span-2;
+  @apply bg-white leading-loose px-4 rounded-lg col-span-2;
 }
 
 .receipt-description-box-text {
