c17f88ed532fd2af6ee6f7f93e39f91b033e7d2e
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-01-03 02:50:08 +0700

    Update input styles and text sizes

--- packages/frontend/src/views/login.vue
+++ packages/frontend/src/views/login.vue
@@ -1,29 +1,31 @@
 <template>
   <div class="login-page">
     <div class="login-page-content">
-      <form class="w-4/5 md:w-3/5 xl:w-1/3 rounded-lg">
+      <form class="w-11/12 md:w-3/5 xl:w-1/3 rounded-lg">
         <div class="bg-pale-yellow shadow-transparent rounded-2xl px-8 pt-6 pb-8 mb-4 flex flex-col">
           <div class="flex font-bold justify-center scroll-mt-50">
             <img class="h-64 w-64 mb-2" src="@/assets/reeba-logo.png" alt="logo-reeba">
           </div>
-          <h3 class="text-2xl font-bold text-white text-center">Login</h3>
+          <h3 class="text-4xl text-white text-center">
+            Login
+          </h3>
           <div class="mb-4">
-            <label class="block text-white text-sm font-sans mb-2" for="email">
+            <label class="block text-pale-gray text-sm font-sans mb-2" for="email">
               Email
             </label>
-            <input class=" shadow-lg shadow-zinc-900 appearance-none border rounded-xl w-full py-2 px-3 text-grey-darker" id="username" type="text" placeholder="Email">
+            <input class=" shadow-lg shadow-zinc-900 rounded-xl w-full py-2 px-3 mb-2 text-grey-darker outline-none focus:ring-pale-gray ring-0 focus:ring-2" id="username" type="text" placeholder="Email">
           </div>
           <div class="mb-6">
-            <label class="block text-white text-sm font-sans mb-2" for="password">
+            <label class="block text-pale-gray text-sm font-sans mb-2" for="password">
               Password
             </label>
-            <input class="shadow-lg shadow-zinc-900 appearance-none border border-red rounded-xl w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="Password">
+            <input class="shadow-lg shadow-zinc-900 border-red rounded-xl w-full py-2 px-3 text-grey-darker mb-3 outline-none focus:ring-pale-gray ring-0 focus:ring-2" id="password" type="password" placeholder="Password">
           </div>
           <a class="inline-block align-baseline font-sans text-right text-white hover:underline" href="#">
             Forgot Password?
           </a>
           <div class="flex items-center justify-center">
-            <button class="bg-pale-gray hover:bg-gray-hover text-white font-sans py-2 px-8 rounded-full" type="button">
+            <button class="bg-pale-gray hover:bg-gray-hover text-white font-sans py-2 px-8 rounded-xl" type="button">
               Sign in
             </button>
           </div>
