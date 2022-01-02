c36b2086da16579ebb77074ffcdb42946af962bc
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-01-03 02:41:59 +0700

    Update login.vue

--- packages/frontend/src/views/login.vue
+++ packages/frontend/src/views/login.vue
@@ -17,7 +17,7 @@
             <label class="block text-white text-sm font-sans mb-2" for="password">
               Password
             </label>
-            <input class="shadow-lg shadow-zinc-900  appearance-none border border-red rounded-xl w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="Password">
+            <input class="shadow-lg shadow-zinc-900 appearance-none border border-red rounded-xl w-full py-2 px-3 text-grey-darker mb-3" id="password" type="password" placeholder="Password">
           </div>
           <a class="inline-block align-baseline font-sans text-right text-white hover:underline" href="#">
             Forgot Password?
