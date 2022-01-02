fded36350f3e63fcab9a730c925342001b004d96
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-01-02 18:00:51 +0700

    Setup upload page for testing

--- packages/frontend/src/views/upload.vue
+++ packages/frontend/src/views/upload.vue
@@ -2,15 +2,49 @@
 <div class="upload-page">
   <div class="upload-page-content">
     <h1 class="text-white text-4xl">upload</h1>
+    <input type="file" class="text-white text-xl" name="avatar-image-input" id="avatar-image-input" @change="onFileUploaded">
+    <button class="w-16 h-9 bg-pale-yellow text-white rounded-xl" @click="uploadFile">Upload</button>
   </div>
 </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import ky from 'ky'
+import { defineComponent, onMounted, ref } from 'vue'
+
+import { postAvatar } from '@/api/endpoints'
 
 export default defineComponent({
-  name: 'upload'
+  name: 'upload',
+  setup () {
+    const file = ref<File | null>(null)
+
+    const onFileUploaded = (event: Event): void => {
+      const inputtedFile = (event.target as HTMLInputElement).files
+
+      file.value = inputtedFile == null ? null : inputtedFile[0]
+    }
+
+    const uploadFile = async (): Promise<void> => {
+      const response = await ky(postAvatar.url, {
+        method: postAvatar.method,
+        json: {
+          hello: 'hello'
+        }
+      }).json<{ hello: string }>()
+
+      console.log('world', response)
+    }
+
+    onMounted(() => {
+      console.log(import.meta.env)
+    })
+
+    return {
+      onFileUploaded,
+      uploadFile
+    }
+  }
 })
 </script>
 
