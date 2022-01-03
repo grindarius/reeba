41c73d28a67cb81a2bc8f60c4f69ccb9c01852a4
Baramee Chaiho <pahe.014700@gmail.com>
2022-01-04 01:53:18 +0700

    Create
    Create
    -Create a user edit page.
    -Create a ticket purchase history page.

--- packages/frontend/src/views/account/account.vue
+++ packages/frontend/src/views/account/account.vue
@@ -1,41 +1,44 @@
 <template>
-  <div class="account-page">
-    <div class="container flex flex-row">
-      <nav class="account-table">
-        <div class="account-image-wrapper">
-          <div class="w-10/12">
-            <img src="@/assets/user.png" class="mx-auto w-20 h-20 rounded-full" />
-          </div>
+<div class="account-page">
+  <div class="container flex flex-row">
+    <nav class="account-table">
+      <div class="account-image-wrapper">
+        <div class="w-10/12">
+          <img src="@/assets/user.png" class="mx-auto w-20 h-20 rounded-full" />
         </div>
-        <div class="text-center">
-          <span class="font-semibold text-white text-lg">Bhattarapong Somwong</span>
-          <p class="text-sm text-pale-yellow">Bhattarapongs@nu.ac.th</p>
-        </div>
-        <div class="account-settings-menu">
-          <ul class="account-settings-menu-wrapper">
-            <li class="account-settings-menu-list">
-              <router-link class="w-full h-full" to="/account">My tickets</router-link>
-            </li>
-            <li class="account-settings-menu-list">
-              <router-link class="w-full h-full" to="/account/purchase-history">Purchase history</router-link>
-            </li>
-            <li class="account-settings-menu-list">
-              <router-link class="w-full h-full" to="/account/edit">Edit profile</router-link>
-            </li>
-            <li class="account-settings-menu-list">
-              <router-link class="w-full h-full" to="/account/organizer">Organizer tools</router-link>
-            </li>
-            <li class="account-settings-menu-list">
-              <router-link class="w-full h-full" to="/account/developer">Developer tools</router-link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-      <div class="account-content">
-        <router-view></router-view>
       </div>
+      <div class="text-center">
+        <span class="font-semibold text-white text-lg">Bhattarapong Somwong</span>
+        <p class="text-sm text-pale-yellow">Bhattarapongs@nu.ac.th</p>
+      </div>
+      <div class="account-settings-menu">
+        <ul class="account-settings-menu-wrapper">
+          <li class="account-settings-menu-list">
+            <router-link class="w-full h-full" to="/account">My tickets</router-link>
+          </li>
+          <li class="account-settings-menu-list">
+            <router-link class="w-full h-full" to="/account/purchase-history">Purchase history</router-link>
+          </li>
+          <li class="account-settings-menu-list">
+            <router-link class="w-full h-full" to="/account/edit">Edit profile</router-link>
+          </li>
+          <li class="account-settings-menu-list">
+            <router-link class="w-full h-full" to="/account/organizer">Organizer tools</router-link>
+          </li>
+          <li class="account-settings-menu-list">
+            <router-link class="w-full h-full" to="/account/developer">Developer tools</router-link>
+          </li>
+          <li class="account-settings-menu-list">
+            <router-link class="w-full h-full" to="#">Logout</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="account-content">
+      <router-view></router-view>
     </div>
   </div>
+</div>
 </template>
 
 <script lang="ts">
--- packages/frontend/src/views/account/purchase-history.vue
+++ packages/frontend/src/views/account/purchase-history.vue
@@ -1,6 +1,23 @@
 <template>
 <div class="purchase-history-page">
-  <h1 class="text-white font-semibold text-4xl">purchase-history</h1>
+  <h1 class="page-header">Purchase History</h1>
+  <section class="content-box-size">
+    <div class="content-box-inside">
+      <div class="content-box-setting-image">
+        <img class="content-box-picture" src="@/assets/photo.jpg" />
+      </div>
+      <div class="content-box-textbox">
+        <p class="content-box-frontsize-title">Rajamangala National Stadium</p>
+        <p class="content-box-frontsize-main">Seat</p>
+        <p class="content-box-frontsize-message">C4-C09</p>
+        <p class="content-box-frontsize-main">Prices</p>
+        <p class="content-box-frontsize-message">6800 THB</p>
+        <p class="content-box-frontsize-main">Show date</p>
+        <p class="content-box-frontsize-message">Wednesday, 6 April 2022</p>
+        <p class="content-box-frontsize-message">Thursday, 7 April 2022</p>
+      </div>
+    </div>
+  </section>
 </div>
 </template>
 
@@ -11,3 +28,42 @@ export default defineComponent({
   name: 'purchase-history'
 })
 </script>
+
+<style scoped lang="scss">
+.page-header {
+  @apply text-4xl font-semibold text-white;
+}
+
+.content-box-frontsize-title {
+  @apply text-sm text-gray-900 font-bold;
+}
+
+.content-box-frontsize-main {
+  @apply text-xs text-gray-900 font-normal;
+}
+
+.content-box-frontsize-message {
+  @apply text-xs text-gray-600 font-normal;
+}
+
+.content-box-size {
+  @apply max-w-md py-4;
+}
+
+.content-box-inside {
+  @apply w-full bg-pale-yellow rounded-lg  overflow-hidden flex flex-col md:flex-row;
+}
+
+.content-box-picture {
+  @apply object-center object-cover h-64 md:h-full w-full;
+}
+
+.content-box-setting-image {
+  @apply md:w-2/5;
+}
+
+.content-box-textbox {
+  @apply w-full md:w-3/5 text-left p-6 md:p-4 space-y-2;
+}
+
+</style>
