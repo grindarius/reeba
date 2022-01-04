74e22d7ddc925dde2e9c83503c6bfd9b66a19f3b
Baramee Chaiho <pahe.014700@gmail.com>
2022-01-04 22:25:30 +0700

    Create Organizer tools.

--- packages/frontend/src/views/account/organizer-tools.vue
+++ packages/frontend/src/views/account/organizer-tools.vue
@@ -1,6 +1,178 @@
 <template>
 <div class="organizer-tools-page">
   <h1 class="page-header">Organizer Tools</h1>
+<div class="flex flex-col mt-8">
+  <div class="setting-header">
+    <div class="header-box">
+        <table class="min-w-full">
+          <thead>
+            <tr>
+              <th
+                class="box-bg">
+                Name</th>
+              <th
+                class="box-bg">
+                Title</th>
+              <th
+                class="box-bg">
+                Status</th>
+              <th
+                class="box-bg">
+                total of tickets</th>
+              <th class="px-6 py-3 border-b border-pale-gray bg-pale-yellow"></th>
+            </tr>
+          </thead>
+
+          <tbody class="bg-pale-yellow">
+            <tr>
+              <td class="px-6 py-4 whitespace-no-wrap border-b border-pale-gray">
+                <div class="flex items-center">
+                  <div class="flex-shrink-0 h-10 w-10">
+                    <img class="h-10 w-10 rounded-full"
+                      src="@/assets/user.png"
+                      alt="">
+                  </div>
+
+                  <div class="ml-4">
+                    <div class="text-sm leading-5 font-medium text-pale-gray">John Doe
+                    </div>
+                    <div class="text-sm leading-5 text-gray-500">john@example.com</div>
+                  </div>
+                </div>
+              </td>
+
+              <td class="px-6 py-4 whitespace-no-wrap border-b border-pale-gray">
+                <div class="text-sm leading-5 text-pale-gray">BTS World Tour 'Love Yourself'</div>
+                <div class="text-sm leading-5 text-gray-500">Rajamangkala National</div>
+              </td>
+
+              <td class="px-6 py-4 whitespace-no-wrap border-b border-pale-gray">
+                <span
+                  class="active-setting">Active</span>
+              </td>
+
+              <td
+                class="px-6 py-4 whitespace-no-wrap border-b border-pale-gray text-sm leading-5 text-pale-gray">
+                1000</td>
+
+              <td
+                class="px-6 py-4 whitespace-no-wrap text-right border-b border-pale-gray text-sm leading-5 font-medium">
+                <a href="#" class="text-pale-gray hover:text-gray-hover">Edit</a>
+              </td>
+            </tr>
+            <tr>
+              <td class="px-6 py-4 whitespace-no-wrap border-b border-pale-gray">
+                <div class="flex items-center">
+                  <div class="flex-shrink-0 h-10 w-10">
+                    <img class="h-10 w-10 rounded-full"
+                      src="@/assets/user.png"
+                      alt="">
+                  </div>
+
+                  <div class="ml-4">
+                    <div class="text-sm leading-5 font-medium text-pale-gray">John Doe
+                    </div>
+                    <div class="text-sm leading-5 text-gray-500">john@example.com</div>
+                  </div>
+                </div>
+              </td>
+
+              <td class="px-6 py-4 whitespace-no-wrap border-b border-pale-gray">
+                <div class="text-sm leading-5 text-pale-gray">Re-MSC: Reconnecting with Mi</div>
+                <div class="text-sm leading-5 text-gray-500">Online Event</div>
+              </td>
+
+              <td class="px-6 py-4 whitespace-no-wrap border-b border-pale-gray">
+                <span
+                  class="active-setting">Active</span>
+              </td>
+
+              <td
+                class="px-6 py-4 whitespace-no-wrap border-b border-pale-gray text-sm leading-5 text-pale-gray ">
+                1000</td>
+
+              <td
+                class="px-6 py-4 whitespace-no-wrap text-right border-b border-pale-gray text-sm leading-5 font-medium">
+                <a href="#" class="text-pale-gray hover:text-gray-hover">Edit</a>
+              </td>
+            </tr>
+            <tr>
+              <td class="px-6 py-4 whitespace-no-wrap border-b border-pale-gray">
+                <div class="flex items-center">
+                  <div class="flex-shrink-0 h-10 w-10">
+                    <img class="h-10 w-10 rounded-full"
+                      src="@/assets/user.png"
+                      alt="">
+                  </div>
+
+                  <div class="ml-4">
+                    <div class="text-sm leading-5 font-medium text-pale-gray">John Doe
+                    </div>
+                    <div class="text-sm leading-5 text-gray-500">john@example.com</div>
+                  </div>
+                </div>
+              </td>
+
+              <td class="px-6 py-4 whitespace-no-wrap border-b border-pale-gray">
+                <div class="text-sm leading-5 text-pale-gray">LIDO CONNECT - Movie Program</div>
+                <div class="text-sm leading-5 text-gray-500">LIDO CONNECT Hall 1</div>
+              </td>
+
+              <td class="px-6 py-4 whitespace-no-wrap border-b border-pale-gray">
+                <span
+                  class="active-setting">Active</span>
+              </td>
+
+              <td
+                class="px-6 py-4 whitespace-no-wrap border-b border-pale-gray text-sm leading-5 text-pale-gray">
+                1000</td>
+
+              <td
+                class="px-6 py-4 whitespace-no-wrap text-right border-b border-pale-gray text-sm leading-5 font-medium">
+                <a href="#" class="text-pale-gray hover:text-gray-hover">Edit</a>
+              </td>
+            </tr>
+            <tr>
+              <td class="px-6 py-4 whitespace-no-wrap border-b border-pale-gray">
+                <div class="flex items-center">
+                  <div class="flex-shrink-0 h-10 w-10">
+                    <img class="h-10 w-10 rounded-full"
+                      src="@/assets/user.png"
+                      alt="">
+                  </div>
+
+                  <div class="ml-4">
+                    <div class="text-sm leading-5 font-medium text-pale-gray">John Doe
+                    </div>
+                    <div class="text-sm leading-5 text-gray-500">john@example.com</div>
+                  </div>
+                </div>
+              </td>
+
+              <td class="px-6 py-4 whitespace-no-wrap border-b border-pale-gray">
+                <div class="text-sm leading-5 text-pale-gray">Sculpture Drive Thru</div>
+                <div class="text-sm leading-5 text-gray-500">Day Day Ari, Ari Samphan Soi 5</div>
+              </td>
+
+              <td class="px-6 py-4 whitespace-no-wrap border-b border-pale-gray">
+                <span
+                  class="active-setting">Active</span>
+              </td>
+
+              <td
+                class="px-6 py-4 whitespace-no-wrap border-b border-pale-gray text-sm leading-5 text-pale-gray">
+                1000</td>
+
+              <td
+                class="px-6 py-4 whitespace-no-wrap text-right border-b border-pale-gray text-sm leading-5 font-medium">
+                <a href="#" class="text-pale-gray hover:text-gray-hover">Edit</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </div>
 </template>
 
@@ -17,4 +189,27 @@ export default defineComponent({
   @apply text-4xl font-semibold text-white;
 }
 
+.box-bg {
+  @apply px-6 py-3 border-b border-pale-gray bg-pale-yellow text-left text-xs leading-4 font-medium text-pale-gray uppercase tracking-wider;
+}
+
+.box-bg-text-header {
+  @apply flex items-center px-5 py-6 shadow-sm rounded-md bg-pale-yellow text-pale-gray;
+}
+
+.box-text-secondary {
+  @apply text-2xl font-semibold text-pale-gray;
+}
+
+.header-box {
+  @apply align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-pale-gray;
+}
+
+.setting-header {
+  @apply -my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8;
+}
+
+.active-setting {
+  @apply px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800;
+}
 </style>
