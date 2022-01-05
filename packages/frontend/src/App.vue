e5b6872e2eb62e2aec371626b1d39f228e6435e7
PhattharapornC <Phattharapornc62@nu.ac.th>
2022-01-05 17:57:00 +0700

    เพิ่มช่องค้นหาและปุ่ม

--- packages/frontend/src/App.vue
+++ packages/frontend/src/App.vue
@@ -4,7 +4,37 @@
       <div class="logo">
         <img src="@/assets/reeba-logo.png" alt="Reeba logo" width="49" height="49" class="logo-image">
       </div>
-    </div>
+      <div class="searchbox">
+        <div class="flex content-center">
+          <div class="hidden relative mr-3 md:mr-0 md:block">
+            <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+              <svg class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+            </div>
+          <input type="text" id="email-adress-icon" class="block p-2 pl-10 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search...">
+          </div>
+        </div>
+      </div>
+      <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+          <a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+            Creat new event
+          </a>
+      </div>
+        <div>
+          <a href="#" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+            Creat new event
+          </a>
+        </div>
+        <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+          <a href="#" class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+            Sign up
+          </a>
+      </div>
+        <div>
+          <a href="#" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+            Sign up
+          </a>
+        </div>
+  </div>
   </nav>
   <router-view></router-view>
 </template>
