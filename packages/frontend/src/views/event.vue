8538ce220fcf183ba7c51c02daa8ffc6f1f2ff69
PawinaK <95273691+PawinaK@users.noreply.github.com>
2022-01-06 12:11:30 +0700

    ใช่แบบนี้มั้ย แก้ปุ่ม Buy ตามเม้น

--- packages/frontend/src/views/event.vue
+++ packages/frontend/src/views/event.vue
@@ -95,16 +95,16 @@
               <div class="show-date-schedule">
                 Wednesday, 6 April 2022
               </div>
-              <button class="buy-button">
-               <router-link to="/select-seat">Buy</router-link>
+              <button>
+                <router-link to="/select-seat" class="buy-button" >Buy</router-link>
               </button>
             </div>
             <div class="show-date">
               <div class="show-date-schedule">
                 Thursday, 7 April 2022
               </div>
-              <button class="buy-button">
-                <router-link to="/select-seat">Buy</router-link>
+              <button>
+                <router-link to="/select-seat" class="buy-button" >Buy</router-link>
               </button>
             </div>
           </div>
@@ -181,6 +181,6 @@ export default defineComponent({
 }
 
 .buy-button {
-  @apply rounded-lg bg-pale-gray text-white px-8 py-2 hover:bg-gray-hover;
+  @apply rounded-lg bg-pale-gray text-white px-8 py-2 hover:bg-gray-hover inline-block;
 }
 </style>
