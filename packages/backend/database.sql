13cef7a31c20f5de568f7f7632a47915fdaad784
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-01-07 18:59:48 +0700

    update database syntax and database default image

--- packages/backend/database.sql
+++ packages/backend/database.sql
@@ -6,10 +6,10 @@ create type t_user_role as enum ('user', 'organizer', 'admin');
 drop database if exists users;
 create table if not exists users(
   user_id text not null,
-  user_name text not null UNIQUE,
-  user_email text not null UNIQUE,
+  user_name text not null unique,
+  user_email text not null unique,
   user_password text not null,
   user_role t_user_role not null default 'user',
-  user_profile_path text not null default './uploads/default-user-profile.png',
+  user_profile_path text not null default '',
   primary key (user_id)
 );
