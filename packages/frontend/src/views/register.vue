be3de7c8db9e4c505ca956094395f34c28d8a9e2
LzzAa35 <62424250+LzzAa35@users.noreply.github.com>
2022-01-14 23:10:06 +0700

    making input and add logo

--- packages/frontend/src/views/register.vue
+++ packages/frontend/src/views/register.vue
@@ -1,9 +1,56 @@
 <template>
   <div class="register-page">
     <div class="register-page-content">
-      <h1 class="text-4xl text-white">
-        register
-      </h1>
+      <form class="w-11/12 rounded-lg">
+        <div class="h-screen min-h-screen flex flex-col items-center justify-cente">
+          <div class="flex flex-col px-8 pt-6 pb-8 mb-4 rounded-2xl bg-pale-yellow shadow-transparent">
+            <div class="flex justify-center font-bold mt-50">
+              <img class="mb-1 w-64 h-64" src="@/assets/reeba-logo.png" alt="logo-reeba">
+            </div>
+            <h1 class="text-4xl text-center text-white">
+              Register
+            </h1>
+            <h2 class="mt-3 text-base text-center text-white">
+              Enter your credentials to get access account
+            </h2>
+            <div class="mt-4">
+              <label class="block mb-2 text-sm text-pale-gray" for="username">
+                Username
+              </label>
+              <div>
+                <input class="py-2 px-3 mb-2 w-full rounded-xl ring-0 shadow-lg outline-none focus:ring-2 shadow-zinc-900 text-grey-darker focus:ring-pale-gray" id="username" type="text" placeholder="Username">
+              </div>
+            </div>
+
+            <div class="mt-4">
+              <label class="block mb-2 text-sm text-pale-gray" for="email">
+                Email
+              </label>
+              <div>
+                <input class="py-2 px-3 mb-2 w-full rounded-xl ring-0 shadow-lg outline-none focus:ring-2 shadow-zinc-900 text-grey-darker focus:ring-pale-gray" id="email" type="text" placeholder="Email">
+              </div>
+            </div>
+
+            <div class="mt-4">
+              <label class="block mb-2 text-sm text-pale-gray" for="password">
+                Password
+              </label>
+              <div>
+                <input class="py-2 px-3 mb-2 w-full rounded-xl ring-0 shadow-lg outline-none focus:ring-2 shadow-zinc-900 border-red text-grey-darker focus:ring-pale-gray" id="password" type="password" placeholder="Password">
+              </div>
+            </div>
+
+            <div class="mt-4">
+              <label class="block mb-2 text-sm text-pale-gray" for="confirm-password">
+                Confirm password
+              </label>
+              <div>
+                <input class="py-2 px-3 mb-2 w-full rounded-xl ring-0 shadow-lg outline-none focus:ring-2 shadow-zinc-900 text-grey-darker focus:ring-pale-gray" id="confirm-password" type="password" placeholder="Confirm password">
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -18,10 +65,10 @@ export default defineComponent({
 
 <style scoped lang="scss">
 .register-page {
-  @apply flex flex-row justify-center w-full min-h-screen bg-pale-gray;
+  @apply w-full h-screen bg-pale-gray;
 }
 
 .register-page-content {
-  @apply container mt-6;
+  @apply flex justify-center items-center w-full h-screen;
 }
 </style>
