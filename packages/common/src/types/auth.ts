bd7338e664b5d48273d916f35ad1594a55857315
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-01-20 16:01:18 +0700

    Refactor reply type of `/auth/signin`

--- packages/common/src/types/auth.ts
+++ packages/common/src/types/auth.ts
@@ -1,5 +1,7 @@
 import { Static, Type } from '@sinclair/typebox'
 
+import { t_user_roles } from '.'
+
 export const SigninParamsSchema = Type.Object({
   email: Type.String(),
   password: Type.String()
@@ -7,7 +9,13 @@ export const SigninParamsSchema = Type.Object({
 export type SigninParams = Static<typeof SigninParamsSchema>
 
 export const SigninReplySchema = Type.Object({
-  token: Type.String()
+  token: Type.String(),
+  username: Type.String(),
+  role: Type.Union([
+    Type.Literal(t_user_roles.user),
+    Type.Literal(t_user_roles.organizer),
+    Type.Literal(t_user_roles.admin)
+  ])
 })
 export type SigninReply = Static<typeof SigninReplySchema>
 
