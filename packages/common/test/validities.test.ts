bc9feb928c01ee6e9d57589b54936f033bd44b39
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-01-20 18:01:51 +0700

    move validate modules to common

--- packages/common/test/validities.test.ts
+++ packages/common/test/validities.test.ts
@@ -1,6 +1,6 @@
 import t from 'tap'
 
-import { validateEmail } from '../src/utils'
+import { validateEmail, validateUsername } from '../src/utils'
 
 void t.test('Validate email formats', async t => {
   t.strictSame(validateEmail('an_email@gmail.com'), true)
@@ -17,4 +17,15 @@ void t.test('Validate email formats', async t => {
   t.strictSame(validateEmail('abc-def@gmail#archive.com'), false)
   t.strictSame(validateEmail('abc-def@gmail.org'), true)
   t.strictSame(validateEmail('noreply@reeba.com'), true)
+})
+
+void t.test('Validate username formats', async t => {
+  t.strictSame(validateUsername('a'), true)
+  t.strictSame(validateUsername('abc'), true)
+  t.strictSame(validateUsername('AbC'), true)
+  t.strictSame(validateUsername('Chang'), true)
+  t.strictSame(validateUsername('Chang international'), false)
+  t.strictSame(validateUsername('Chang_international'), true)
+  t.strictSame(validateUsername(' sdfdf'), false)
+  t.strictSame(validateUsername('123sdfdf'), false)
 })
