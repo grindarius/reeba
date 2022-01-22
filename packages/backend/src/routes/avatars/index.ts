efd2ab789c51a8688416808d5005dc798a2c2c80
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-01-22 12:42:55 +0700

    Write checks for querying user avatar

--- packages/backend/src/routes/avatars/index.ts
+++ packages/backend/src/routes/avatars/index.ts
@@ -1,14 +1,40 @@
-import { FastifyInstance, FastifyPluginOptions } from 'fastify'
+import { FastifyInstance, FastifyPluginOptions, FastifySchema } from 'fastify'
 import { createWriteStream } from 'fs'
 import { pipeline } from 'stream'
 import { promisify } from 'util'
 
+import { GetAvatarsParams, GetAvatarsParamsSchema, users } from '@reeba/common'
+
 const pump = promisify(pipeline)
 
+const schema: FastifySchema = {
+  params: GetAvatarsParamsSchema
+}
+
 export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promise<void> => {
-  instance.get('/', async (_, reply) => {
-    return await reply.sendFile('default-user-profile.png')
-  })
+  instance.get<{ Params: GetAvatarsParams }>(
+    '/:username',
+    { schema },
+    async (request, reply) => {
+      const { username } = request.params
+
+      if (username == null || username === '') {
+        return await reply.sendFile('/uploads/default-user-profile.png')
+      }
+
+      const imagePath = await instance.pg.query<Pick<users, 'user_image_profile_path'>, [users['user_username']]>(
+        'select user_image_profile_path from users where user_username = $1',
+        [username]
+      )
+
+      if (imagePath.rows.length === 0) {
+        return await reply.sendFile('/uploads/default-user-profile.png')
+      }
+
+      return await reply.sendFile('/uploads/' + imagePath.rows[0].user_image_profile_path)
+    }
+  )
+
   instance.post('/', async (request, _) => {
     const data = await request.file()
 
