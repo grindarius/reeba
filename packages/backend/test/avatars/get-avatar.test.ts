370843ee59b404055edb1996e69cc24517a6296e
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-02-01 12:46:36 +0700

    separate test files

--- packages/backend/test/avatars/get-avatar.test.ts
+++ packages/backend/test/avatars/get-avatar.test.ts
@@ -20,7 +20,7 @@ const client = new Client({
   database: process.env.POSTGRES_DBNAME
 })
 
-void t.test('post image', async t => {
+void t.test('get image', async t => {
   const app = createServer()
 
   t.teardown(async () => {
@@ -32,83 +32,33 @@ void t.test('post image', async t => {
     await client.connect()
 
     // * Looking for existing logged in user, if not. create one (without image path).
-    const email = await client.query('select * from "users" where user_username = \'avatar_test\'')
+    const email = await client.query('select * from "users" where user_username = \'getavatartest\'')
 
     if (email.rowCount <= 0) {
       await app.inject({
         method: 'POST',
         url: '/auth/signup',
         payload: {
-          username: 'avatar_test',
-          email: 'avatartest@gmail.com',
-          password: 'avatartest_123'
+          username: 'getavatartest',
+          email: 'getavatartest@gmail.com',
+          password: 'getavatartest_123'
         }
       })
     }
+
+    const form = new FormData()
+    form.append('image', createReadStream(resolve(__dirname, 'test-get-avatar.png')))
+
+    await app.inject({
+      method: 'POST',
+      url: '/avatars/getavatartest',
+      payload: form,
+      headers: form.getHeaders()
+    })
   } catch (error) {
     t.error(error)
     t.fail('Error while connecting to database')
   }
-
-  void t.test('passing empty string as username', async t => {
-    try {
-      const response = await app.inject({
-        method: 'POST',
-        url: '/avatars/'
-      })
-
-      t.strictSame(response.statusCode, 400, 'status code from posting without username')
-      t.strictSame(response.json().message, 'params should have required property \'username\'', 'error message')
-    } catch (error) {
-      t.error(error)
-      t.fail('There should not be an error')
-    }
-  })
-
-  void t.test('passing unknown username', async t => {
-    try {
-      const response = await app.inject({
-        method: 'POST',
-        url: '/avatars/unknown_username'
-      })
-
-      t.strictSame(response.statusCode, 404, 'status code from posting without username')
-      t.strictSame(response.json().message, 'user not found', 'error message')
-    } catch (error) {
-      t.error(error)
-      t.fail('There should not be an error')
-    }
-  })
-
-  void t.test('posting image to existing profile', async t => {
-    const form = new FormData()
-
-    form.append('image', createReadStream(resolve(__dirname, 'test-avatar.jpg')))
-
-    try {
-      const response = await app.inject({
-        method: 'POST',
-        url: '/avatars/avatar_test',
-        payload: form,
-        headers: form.getHeaders()
-      })
-
-      t.strictSame(response.statusCode, 200, 'status code from posting image')
-      t.strictSame(response.json().message, 'complete', 'message from posting image')
-    } catch (error) {
-      t.error(error)
-      t.fail('Error while posting image to existing profile')
-    }
-  })
-})
-
-void t.test('Get image', async t => {
-  const app = createServer()
-
-  t.teardown(async () => {
-    await app.close()
-    await client.end()
-  })
 
   void t.test('get default user image when emptystring is passed', async t => {
     try {
@@ -119,7 +69,7 @@ void t.test('Get image', async t => {
 
       Resemble(response.rawPayload).compareTo(readFileSync(resolve(__dirname, '..', '..', 'uploads', 'default-user-profile.png'))).onComplete((result) => {
         t.strictSame(result.isSameDimensions, true, 'same image dimension')
-        t.strictSame(result.misMatchPercentage, '0.00', 'image mismatch percentage')
+        t.strictSame(Number(result.misMatchPercentage), 0, 'image mismatch percentage')
       })
     } catch (error) {
       t.error(error)
@@ -137,7 +87,7 @@ void t.test('Get image', async t => {
       Resemble(response.rawPayload).compareTo(readFileSync(resolve(__dirname, '..', '..', 'uploads', 'default-user-profile.png')))
         .onComplete(result => {
           t.strictSame(result.isSameDimensions, true, 'same image dimension')
-          t.strictSame(result.misMatchPercentage, '0.00', 'image mismatch percentage')
+          t.strictSame(Number(result.misMatchPercentage), 0, 'image mismatch percentage')
         })
     } catch (error) {
       t.error(error)
@@ -145,17 +95,17 @@ void t.test('Get image', async t => {
     }
   })
 
-  void t.test('get avatar of user avatar_test', async t => {
+  void t.test('get avatar of user getavatartest', async t => {
     try {
       const response = await app.inject({
         method: 'GET',
-        path: '/avatars/avatar_test'
+        path: '/avatars/getavatartest'
       })
 
-      Resemble(response.rawPayload).compareTo(readFileSync(resolve(__dirname, 'test-avatar.jpg')))
+      Resemble(response.rawPayload).compareTo(readFileSync(resolve(__dirname, 'test-get-avatar.png')))
         .onComplete(result => {
           t.strictSame(result.isSameDimensions, true, 'same image dimension')
-          t.strictSame(result.misMatchPercentage, '0.00', 'image mismatch percentage')
+          t.strictSame(Number(result.misMatchPercentage), 0, 'image mismatch percentage')
         })
     } catch (error) {
       t.error(error)
@@ -173,7 +123,7 @@ void t.test('Get image', async t => {
       Resemble(response.rawPayload).compareTo(readFileSync(resolve(__dirname, '..', '..', 'uploads', 'default-user-profile.png')))
         .onComplete(result => {
           t.strictSame(result.isSameDimensions, true, 'same image dimension')
-          t.strictSame(result.misMatchPercentage, '0.00', 'image mismatch percentage')
+          t.strictSame(Number(result.misMatchPercentage), 0, 'image mismatch percentage')
         })
     } catch (error) {
       t.error(error)
