cdd2e63d160dbf6d863b1aa40c1f13f7985e7b11
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-02-01 12:48:06 +0700

    new function to test for valid filename

--- packages/common/src/constants/regexp.ts
+++ packages/common/src/constants/regexp.ts
@@ -9,3 +9,10 @@ export const emailRegExp: RegExp = /[\w-]+@\w+\.[a-zA-Z]+/
  * @see https://stackoverflow.com/questions/12018245/regular-expression-to-validate-username
  */
 export const usernameRegExp: RegExp = /^(?=.{0,30}$)[a-zA-Z0-9_-]+$/
+
+/**
+ * RegExp used to find file extension to a filename and invalidate file extensions
+ *
+ * @see https://stackoverflow.com/a/374956/12386405
+ */
+export const fileExtensionMatchRegExp: RegExp = /^.*\.(jpg|JPG|png|PNG|jpeg|JPEG)$/
