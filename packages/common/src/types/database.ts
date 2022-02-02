6d601e4ff19d22fd94bf071b085ca4d462ef114a
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-02-02 13:39:05 +0700

    update type definitions for database

--- packages/common/src/types/database.ts
+++ packages/common/src/types/database.ts
@@ -20,6 +20,15 @@ export interface t_event_price {
   seat_price: number
 }
 
+/**
+ * Enum to store event status
+ *
+ * - `'open'` means the event is open to others, will be able to look at and make transactions.
+ * - `'closed'` means the event is not accepting anymore transactions, and will not be visible to others.
+ * Will be marked as closed for other people who bought the ticket.
+ *
+ * TODO: Might need more statuses
+ */
 export const enum t_event_status {
   open = 'open',
   closed = 'closed'
@@ -118,63 +127,151 @@ export interface events {
    */
   event_venue_coordinates: point
   /**
-   * Event's opening date to sell tickets, stored as timestamp with time zone string, not default
+   * Event's opening date to sell tickets, stored as `timestamp with time zone` string, no default
    * but will never be null.
    */
   event_opening_date: string
-  event_status: string
+  /**
+   * Event's status, either `'open'` or `'closed'`. default for when opening an event is `closed`.
+   */
+  event_status: t_event_status
+  /**
+   * Event's ticket prices array. Will be an array of type `t_event_price`. Stored as a pair of
+   * color and their price as integer.
+   */
   event_ticket_prices: Array<t_event_price>
+  /**
+   * Minimum age of a user, cannot be null, cannot be less than 0, default is 0
+   */
   event_minimum_age: number
 }
 
 /**
- * Table storing a `n:m` relationship between event and its tags
+ * Table that stores tags of an event. needs HEAVY NORMALIZATION before putting in.
  */
 export interface event_tags {
+  /**
+   * Tag label, primark key. Refers to type of an event.
+   */
   event_tag_label: string
-}
-
-export interface event_tags_bridge {
-  event_tag_label: string
-  event_id: string
-}
-
-export interface event_datetimes {
-  event_datetime_id: string
-  event_id: string
-  event_start_datetime: string
-  event_end_datetime: string
-}
-
-export interface event_sections {
-  event_section_id: string
-  event_datetime_id: string
-  event_section_row_position: number
-  event_section_column_position: number
-}
-
-export interface event_seats {
-  event_seat_id: string
-  event_section_id: string
-  event_seat_price: number
-  event_seat_row_position: number
-  event_seat_column_position: number
-}
-
-export interface transactions {
-  transaction_id: string
-  user_username: string
-  event_id: string
-}
-
-export interface transaction_details {
-  event_seat_id: string
-  transaction_id: string
 }
 
 /**
- * Table storing tags of events created by users.
+ * table that creates a many to many relationship between an event and their tags.
  */
-export interface tags {
-  tag_label: string
+export interface event_tags_bridge {
+  /**
+   * Tag label, primark key. Refers to type of an event.
+   */
+  event_tag_label: string
+  /**
+   * Event's ID
+   */
+  event_id: string
+}
+
+/**
+ * Table that stores datetimes of an event. (1 event could occur many times)
+ */
+export interface event_datetimes {
+  /**
+   * Datetime id, default is `nanoid()`
+   */
+  event_datetime_id: string
+  /**
+   * Event's id
+   */
+  event_id: string
+  /**
+   * Event start datetime, refers to when an event is starting.
+   */
+  event_start_datetime: string
+  /**
+   * Event's end datetime, referes to when an event is ending.
+   */
+  event_end_datetime: string
+}
+
+/**
+ * Event seat section (a group of many seats)
+ */
+export interface event_sections {
+  /**
+   * Section id
+   */
+  event_section_id: string
+  /**
+   * Datetime id
+   */
+  event_datetime_id: string
+  /**
+   * row position, the section is constructed in 2d manner, zero-based
+   */
+  event_section_row_position: number
+  /**
+   * column position, the section is constructed in 2d manner, zero-based
+   */
+  event_section_column_position: number
+}
+
+/**
+ * Table storing each individual seats in an event
+ */
+export interface event_seats {
+  /**
+   * Seat id.
+   */
+  event_seat_id: string
+  /**
+   * its section it belongs to.
+   */
+  event_section_id: string
+  /**
+   * Seat price.
+   */
+  event_seat_price: number
+  /**
+   * seat row position, the seat is constructed in 2d manner. zero-based
+   */
+  event_seat_row_position: number
+  /**
+   * seat column position, the seat is constructed in 2d manner. zero-based
+   */
+  event_seat_column_position: number
+}
+
+/**
+ * Table storing each transaction
+ */
+export interface transactions {
+  /**
+   * transaction id
+   */
+  transaction_id: string
+  /**
+   * user who does the transaction
+   */
+  user_username: string
+  /**
+   * which event the transaction belongs to.
+   */
+  event_id: string
+  /**
+   * time when the transaction happens. stores in pg timestamp with time zone format.
+   */
+  transaction_time: string
+}
+
+/**
+ * table storing individual seats the transactions happen.
+ */
+export interface transaction_details {
+  /**
+   * which seat this transaction has acquired.
+   */
+  event_seat_id: string
+  /**
+   * which transaction id it belongs to.
+   */
+  transaction_id: string
 }
