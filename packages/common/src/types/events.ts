1f916b5989775e0f974d9816a518c90321b71f69
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-02-05 01:10:48 +0700

    update body schema of event

--- packages/common/src/types/events.ts
+++ packages/common/src/types/events.ts
@@ -11,5 +11,19 @@ export const PostEventsBodySchema = Type.Object({
     y: Type.Number()
   }),
   eventOpeningDate: Type.String(),
-  event
+  eventTicketPrices: Type.Array(Type.Object({
+    color: Type.String(),
+    price: Type.Number()
+  })),
+  eventMinimumAge: Type.Number(),
+  sections: Type.Array(Type.Array(Type.Object({
+    sectionRowPosition: Type.Number(),
+    sectionColumnPosition: Type.Number(),
+    seats: Type.Array(Type.Array(Type.Object({
+      seatRowPosition: Type.Number(),
+      seatColumnPosition: Type.Number(),
+      seatPrice: Type.Number()
+    })))
+  })))
 })
+export type PostEventsBody = Static<typeof PostEventsBodySchema>
