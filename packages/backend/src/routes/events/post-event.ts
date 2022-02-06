871894eda8b4d8fc16b88224ddca1c176097aa66
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-02-07 02:39:32 +0700

    update insert codes

--- packages/backend/src/routes/events/post-event.ts
+++ packages/backend/src/routes/events/post-event.ts
@@ -1,10 +1,20 @@
+import dayjs from 'dayjs'
+import customParseFormat from 'dayjs/plugin/customParseFormat'
 import { FastifyInstance, FastifyPluginOptions, FastifySchema } from 'fastify'
+import { nanoid } from 'nanoid'
 
 import {
   BadRequestReplySchema,
+  event_datetimes,
+  event_seats,
+  event_sections,
+  events,
   PostEventBody,
-  PostEventBodySchema
+  PostEventBodySchema,
+  t_event_status
 } from '@reeba/common'
+
+dayjs.extend(customParseFormat)
 
 const schema: FastifySchema = {
   body: PostEventBodySchema,
@@ -19,11 +29,21 @@ export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promi
     {
       schema,
       preValidation: async (request, reply) => {
-        const { username, eventName, eventDescription, eventWebsite, eventVenueName, eventVenueCoordinates, eventOpeningDate, eventTicketPrices, eventMinimumAge } = request.body
+        const {
+          createdBy,
+          eventName,
+          description,
+          website,
+          venueName,
+          venueCoordinates,
+          openingDate,
+          ticketPrices,
+          minimumAge
+        } = request.body
 
-        if (username == null || username === '') {
+        if (createdBy == null || createdBy === '') {
           void reply.code(400)
-          throw new Error('body should have required property \'username\'')
+          throw new Error('body should have required property \'createdBy\'')
         }
 
         if (eventName == null || eventName === '') {
@@ -31,43 +51,152 @@ export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promi
           throw new Error('body should have required property \'eventName\'')
         }
 
-        if (eventDescription == null) {
+        if (description == null) {
           void reply.code(400)
-          throw new Error('body should have required property \'eventDescription\'')
+          throw new Error('body should have required property \'description\'')
         }
 
-        if (eventWebsite == null) {
+        if (website == null) {
           void reply.code(400)
           throw new Error('body should have required property \'eventWebSite\'')
         }
 
-        if (eventVenueName == null || eventVenueName === '') {
+        if (venueName == null || venueName === '') {
           void reply.code(400)
-          throw new Error('body should have required property \'eventVenueName\'')
+          throw new Error('body should have required property \'venueName\'')
         }
 
-        if (eventVenueCoordinates == null) {
+        if (venueCoordinates == null) {
           void reply.code(400)
           throw new Error('body should have required property \'eventVenuCoordinates\'')
         }
 
-        if (eventOpeningDate == null || eventOpeningDate === '') {
+        if (openingDate == null || openingDate === '') {
           void reply.code(400)
-          throw new Error('body should have required property \'eventOpeningDate\'')
+          throw new Error('body should have required property \'openingDate\'')
         }
 
-        if (eventTicketPrices == null) {
+        if (ticketPrices == null || !Array.isArray(ticketPrices) || ticketPrices.length === 0) {
           void reply.code(400)
-          throw new Error('body should have required property \'eventTicketPrices\'')
+          throw new Error('body should have required property \'ticketPrices\'')
         }
 
-        if (eventMinimumAge == null || eventMinimumAge < 0) {
+        if (minimumAge == null || minimumAge < 0) {
           void reply.code(400)
-          throw new Error('body should have required property \'eventMinimumAge\'')
+          throw new Error('body should have required property \'minimumAge\'')
         }
       }
-    }, async (request, reply) => {
-      const { username, eventName, eventDescription, eventWebsite, eventVenueName, eventVenueCoordinates, eventOpeningDate, eventTicketPrices, eventMinimumAge } = request.body
+    }, async (request) => {
+      const {
+        createdBy,
+        eventName,
+        description,
+        website,
+        venueName,
+        venueCoordinates,
+        openingDate,
+        ticketPrices,
+        minimumAge,
+        sections,
+        datetimes
+      } = request.body
+
+      type InsertEvent = [
+        events['event_id'],
+        events['user_username'],
+        events['event_name'],
+        events['event_description'],
+        events['event_website'],
+        events['event_venue_name'],
+        events['event_venue_coordinates'],
+        events['event_creation_date'],
+        events['event_opening_date'],
+        events['event_status'],
+        events['event_ticket_prices'],
+        events['event_minimum_age']
+      ]
+
+      const eventId = await instance.pg.query<Pick<events, 'event_id'>, InsertEvent>(
+        `insert into events (
+          event_id,
+          user_username,
+          event_name,
+          event_description,
+          event_website,
+          event_venue_name,
+          event_venue_coordinates,
+          event_creation_date,
+          event_opening_date,
+          event_status,
+          event_ticket_prices,
+          event_minimum_age
+        ) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11::t_event_price[], $12) returning event_id`,
+        [
+          nanoid(),
+          createdBy,
+          eventName,
+          description,
+          website,
+          venueName,
+          venueCoordinates,
+          dayjs().format('YYYY-MM-DD HH:mm:ss.SSS Z'),
+          openingDate,
+          t_event_status.open,
+          ticketPrices.map(price => { return { price_color: price.color, price_value: price.price } }),
+          minimumAge
+        ]
+      )
+
+      type InsertDatetimes = [
+        event_datetimes['event_datetime_id'],
+        event_datetimes['event_id'],
+        event_datetimes['event_start_datetime'],
+        event_datetimes['event_end_datetime']
+      ]
+
+      type InsertSection = [
+        event_sections['event_section_id'],
+        event_sections['event_datetime_id'],
+        event_sections['event_section_row_position'],
+        event_sections['event_section_column_position']
+      ]
+
+      type InsertSeat = [
+        event_seats['event_seat_id'],
+        event_seats['event_section_id'],
+        event_seats['event_seat_price'],
+        event_seats['event_seat_row_position'],
+        event_seats['event_seat_column_position']
+      ]
+
+      for await (const datetime of datetimes) {
+        const datetimeId = await instance.pg.query<Pick<event_datetimes, 'event_datetime_id'>, InsertDatetimes>(
+          'insert into event_datetimes (event_datetime_id, event_id, event_start_datetime, event_end_datetime) values ($1, $2, $3, $4) returning event_datetime_id',
+          [nanoid(), eventId.rows[0].event_id, datetime.start, datetime.end]
+        )
+
+        for await (const sectionRow of sections) {
+          for await (const section of sectionRow) {
+            const sectionId = await instance.pg.query<Pick<event_sections, 'event_section_id'>, InsertSection>(
+              'insert into event_sections (event_section_id, event_datetime_id, event_section_row_position, event_section_column_position) values ($1, $2, $3, $4) returning event_section_id',
+              [nanoid(), datetimeId.rows[0].event_datetime_id, section.sectionRowPosition, section.sectionColumnPosition]
+            )
+
+            for await (const seatRow of section.seats) {
+              for await (const seat of seatRow) {
+                await instance.pg.query<event_seats, InsertSeat>(
+                  'insert into event_seats (event_seat_id, event_section_id, event_seat_price, event_seat_row_position, event_seat_column_position) values ($1, $2, $3, $4, $5)',
+                  [nanoid(), sectionId.rows[0].event_section_id, seat.seatPrice, seat.seatRowPosition, seat.seatColumnPosition]
+                )
+              }
+            }
+          }
+        }
+      }
+
+      return {
+        message: 'complete'
+      }
     }
   )
 }
