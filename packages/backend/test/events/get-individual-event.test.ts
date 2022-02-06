7c06bb20737217690c48b5a40879ed1387951d00
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-02-07 03:28:52 +0700

    add code to mock event creation

--- packages/backend/test/events/get-individual-event.test.ts
+++ packages/backend/test/events/get-individual-event.test.ts
@@ -1,7 +1,11 @@
+import dayjs from 'dayjs'
 import dotenv from 'dotenv-flow'
+import { nanoid } from 'nanoid'
 import { resolve } from 'node:path'
 import { Client } from 'pg'
 import t from 'tap'
+
+import { event_seats, events, t_event_status } from '@reeba/common'
 
 import createServer from '../../src/app'
 
@@ -177,6 +181,67 @@ const mockEvent = async (): Promise<void> => {
       ]
     ]
   }
+
+  // * check if the event already existed, if no, have to add new one.
+  const targetEvent = await client.query<events, [string]>('select * from "events" where event_id = $1', ['grindarius_event_test'])
+
+  if (targetEvent.rowCount === 0) {
+    const eventId = await client.query<{ event_id: string }>(
+      `insert into events (
+          event_id,
+          user_username,
+          event_name,
+          event_description,
+          event_website,
+          event_venue_name,
+          event_venue_coordinates,
+          event_creation_date,
+          event_opening_date,
+          event_status,
+          event_ticket_prices,
+          event_minimum_age
+        ) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11::t_event_price[], $12) returning event_id`,
+      [
+        nanoid(),
+        ev.createdBy,
+        ev.eventName,
+        ev.description,
+        ev.website,
+        ev.venueName,
+        ev.venueCoordinates,
+        dayjs().format('YYYY-MM-DD HH:mm:ss.SSS Z'),
+        ev.openingDate,
+        t_event_status.open,
+        ev.ticketPrices.map(price => { return { price_color: price.color, price_value: price.price } }),
+        ev.minimumAge
+      ]
+    )
+
+    for await (const datetime of ev.datetimes) {
+      const datetimeId = await client.query<{ event_datetime_id: string }>(
+        'insert into event_datetimes (event_datetime_id, event_id, event_start_datetime, event_end_datetime) values ($1, $2, $3, $4) returning event_datetime_id',
+        [nanoid(), eventId.rows[0].event_id, datetime.start, datetime.end]
+      )
+
+      for await (const sectionRow of ev.sections) {
+        for await (const section of sectionRow) {
+          const sectionId = await client.query<{ event_section_id: string }>(
+            'insert into event_sections (event_section_id, event_datetime_id, event_section_row_position, event_section_column_position) values ($1, $2, $3, $4) returning event_section_id',
+            [nanoid(), datetimeId.rows[0].event_datetime_id, section.sectionRowPosition, section.sectionColumnPosition]
+          )
+
+          for await (const seatRow of section.seats) {
+            for await (const seat of seatRow) {
+              await client.query<event_seats>(
+                'insert into event_seats (event_seat_id, event_section_id, event_seat_price, event_seat_row_position, event_seat_column_position) values ($1, $2, $3, $4, $5)',
+                [nanoid(), sectionId.rows[0].event_section_id, seat.seatPrice, seat.seatRowPosition, seat.seatColumnPosition]
+              )
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 void t.todo('get individual event', async t => {
@@ -189,22 +254,7 @@ void t.todo('get individual event', async t => {
 
   try {
     await client.connect()
-
     await mockEvent()
-
-    const email = await client.query('select * from "users" where user_email = \'logintest@gmail.com\'')
-
-    if (email.rowCount <= 0) {
-      await app.inject({
-        method: 'POST',
-        url: '/auth/signup',
-        payload: {
-          username: 'login_test_boy',
-          email: 'logintest@gmail.com',
-          password: 'logintest_123'
-        }
-      })
-    }
   } catch (error) {
     t.error(error)
     t.fail()
