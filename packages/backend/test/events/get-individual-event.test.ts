7791e34bd5f9f72b6b2e2fca8768d06c32d1ef93
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-02-07 03:19:13 +0700

    event data mock

--- packages/backend/test/events/get-individual-event.test.ts
+++ packages/backend/test/events/get-individual-event.test.ts
@@ -18,6 +18,165 @@ const client = new Client({
 })
 
 const mockEvent = async (): Promise<void> => {
+  const ev = {
+    eventName: 'BTS Army',
+    createdBy: 'grindarius',
+    description: '## No description provided',
+    website: 'www.github.com/sindresorhus/ky',
+    venueName: 'Rajamangkala Stadium',
+    venueCoordinates: {
+      x: '13.755313892097984',
+      y: '100.62221451070221'
+    },
+    openingDate: '2021-03-01 12:00:00.000 +07:00',
+    ticketPrices: [
+      {
+        color: '#4C9141',
+        price: 1000
+      },
+      {
+        color: '#C1876B',
+        price: 1500
+      }
+    ],
+    datetimes: [
+      {
+        start: '2021-03-07 20:00:00.000 +07:00',
+        end: '2021-03-08 00:00:00.000 +07:00'
+      },
+      {
+        start: '2021-03-08 20:00:00.000 +07:00',
+        end: '2021-03-09 00:00:00.000 +07:00'
+      }
+    ],
+    minimumAge: 18,
+    sections: [
+      [
+        {
+          sectionRowPosition: 0,
+          sectionColumnPosition: 0,
+          seats: [
+            [
+              {
+                seatRowPosition: 0,
+                seatColumnPosition: 0,
+                seatPrice: 1500
+              },
+              {
+                seatRowPosition: 0,
+                seatColumnPosition: 1,
+                seatPrice: 1500
+              }
+            ],
+            [
+              {
+                seatRowPosition: 1,
+                seatColumnPosition: 0,
+                seatPrice: 1000
+              },
+              {
+                seatRowPosition: 1,
+                seatColumnPosition: 1,
+                seatPrice: 1000
+              }
+            ]
+          ]
+        },
+        {
+          sectionRowPosition: 0,
+          sectionColumnPosition: 1,
+          seats: [
+            [
+              {
+                seatRowPosition: 0,
+                seatColumnPosition: 0,
+                seatPrice: 1500
+              },
+              {
+                seatRowPosition: 0,
+                seatColumnPosition: 1,
+                seatPrice: 1500
+              }
+            ],
+            [
+              {
+                seatRowPosition: 1,
+                seatColumnPosition: 0,
+                seatPrice: 1000
+              },
+              {
+                seatRowPosition: 1,
+                seatColumnPosition: 1,
+                seatPrice: 1000
+              }
+            ]
+          ]
+        }
+      ],
+      [
+        {
+          sectionRowPosition: 1,
+          sectionColumnPosition: 0,
+          seats: [
+            [
+              {
+                seatRowPosition: 0,
+                seatColumnPosition: 0,
+                seatPrice: 1500
+              },
+              {
+                seatRowPosition: 0,
+                seatColumnPosition: 1,
+                seatPrice: 1500
+              }
+            ],
+            [
+              {
+                seatRowPosition: 1,
+                seatColumnPosition: 0,
+                seatPrice: 1000
+              },
+              {
+                seatRowPosition: 1,
+                seatColumnPosition: 1,
+                seatPrice: 1000
+              }
+            ]
+          ]
+        },
+        {
+          sectionRowPosition: 1,
+          sectionColumnPosition: 1,
+          seats: [
+            [
+              {
+                seatRowPosition: 0,
+                seatColumnPosition: 0,
+                seatPrice: 1500
+              },
+              {
+                seatRowPosition: 0,
+                seatColumnPosition: 1,
+                seatPrice: 1500
+              }
+            ],
+            [
+              {
+                seatRowPosition: 1,
+                seatColumnPosition: 0,
+                seatPrice: 1000
+              },
+              {
+                seatRowPosition: 1,
+                seatColumnPosition: 1,
+                seatPrice: 1000
+              }
+            ]
+          ]
+        }
+      ]
+    ]
+  }
 }
 
 void t.todo('get individual event', async t => {
