8f269b570762341f43cb8743ad5faff58899182e
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-02-12 01:38:08 +0700

    Refactor most of table code

--- packages/frontend/src/views/account/devtools/devtool-users.vue
+++ packages/frontend/src/views/account/devtools/devtool-users.vue
@@ -4,202 +4,100 @@
       <h1 class="text-4xl font-semibold text-white">
         Users
       </h1>
-      <div class="flex flex-col mt-8">
-        <div class="overflow-x-auto py-2 -my-2 sm:px-6 sm:-mx-6 lg:px-8 lg:-mx-8">
-          <div class="inline-block overflow-hidden min-w-full align-middle border-b border-gray-200 shadow sm:rounded-lg">
-            <table class="min-w-full">
-              <thead>
-                <tr>
-                  <th class="box-bg">
-                    Name
-                  </th>
-                  <th class="box-bg">
-                    Title
-                  </th>
-                  <th class="box-bg">
-                    Status
-                  </th>
-                  <th class="box-bg">
-                    Role
-                  </th>
-                  <th class="py-3 px-6 border-b border-pale-gray bg-pale-yellow" />
-                </tr>
-              </thead>
-              <tbody class="bg-pale-yellow">
-                <tr>
-                  <td class="py-4 px-6 whitespace-nowrap border-b border-pale-gray">
-                    <div class="flex items-center">
-                      <div class="flex-shrink-0 w-10 h-10">
-                        <img class="w-10 h-10 rounded-full" src="@/assets/user.png" alt="">
-                      </div>
-                      <div class="ml-4">
-                        <div class="text-sm font-medium leading-5 text-pale-gray">
-                          John Doe
-                        </div>
-                        <div class="text-sm leading-5 text-gray-500">
-                          john@example.com
-                        </div>
-                      </div>
-                    </div>
-                  </td>
-                  <td class="py-4 px-6 whitespace-nowrap border-b border-pale-gray">
-                    <div class="text-sm leading-5 text-pale-gray">
-                      Software Engineer
-                    </div>
-                    <div class="text-sm leading-5 text-gray-500">
-                      Web dev
-                    </div>
-                  </td>
-                  <td class="py-4 px-6 whitespace-nowrap border-b border-pale-gray">
-                    <span class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">
-                      Active
-                    </span>
-                  </td>
-                  <td class="py-4 px-6 text-sm leading-5 whitespace-nowrap border-b border-pale-gray text-pale-gray">
-                    Owner
-                  </td>
-                  <td class="py-4 px-6 text-sm font-medium leading-5 text-right whitespace-nowrap border-b border-pale-gray">
-                    <a href="#" class="text-pale-gray hover:text-gray-hover">
-                      Edit
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="py-4 px-6 whitespace-nowrap border-b border-pale-gray">
-                    <div class="flex items-center">
-                      <div class="flex-shrink-0 w-10 h-10">
-                        <img
-                          class="w-10 h-10 rounded-full"
-                          src="@/assets/user.png"
-                          alt="">
-                      </div>
-                      <div class="ml-4">
-                        <div class="text-sm font-medium leading-5 text-pale-gray">
-                          John Doe
-                        </div>
-                        <div class="text-sm leading-5 text-gray-500">
-                          john@example.com
-                        </div>
-                      </div>
-                    </div>
-                  </td>
-                  <td class="py-4 px-6 whitespace-nowrap border-b border-pale-gray">
-                    <div class="text-sm leading-5 text-pale-gray">
-                      Software Engineer
-                    </div>
-                    <div class="text-sm leading-5 text-gray-500">
-                      Web dev
-                    </div>
-                  </td>
-                  <td class="py-4 px-6 whitespace-nowrap border-b border-pale-gray">
-                    <span class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
-                  </td>
-                  <td class="py-4 px-6 text-sm leading-5 whitespace-nowrap border-b border-pale-gray text-pale-gray">
-                    Owner
-                  </td>
-                  <td class="py-4 px-6 text-sm font-medium leading-5 text-right whitespace-nowrap border-b border-pale-gray">
-                    <a href="#" class="text-pale-gray hover:text-gray-hover">Edit</a>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="py-4 px-6 whitespace-nowrap border-b border-pale-gray">
-                    <div class="flex items-center">
-                      <div class="flex-shrink-0 w-10 h-10">
-                        <img
-                          class="w-10 h-10 rounded-full"
-                          src="@/assets/user.png"
-                          alt="">
-                      </div>
-                      <div class="ml-4">
-                        <div class="text-sm font-medium leading-5 text-pale-gray">
-                          John Doe
-                        </div>
-                        <div class="text-sm leading-5 text-gray-500">
-                          john@example.com
-                        </div>
-                      </div>
-                    </div>
-                  </td>
-                  <td class="py-4 px-6 whitespace-nowrap border-b border-pale-gray">
-                    <div class="text-sm leading-5 text-pale-gray">
-                      Software Engineer
-                    </div>
-                    <div class="text-sm leading-5 text-gray-500">
-                      Web dev
-                    </div>
-                  </td>
-                  <td class="py-4 px-6 whitespace-nowrap border-b border-pale-gray">
-                    <span
-                      class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
-                  </td>
-                  <td
-                    class="py-4 px-6 text-sm leading-5 whitespace-nowrap border-b border-pale-gray text-pale-gray">
-                    Owner
-                  </td>
-                  <td
-                    class="py-4 px-6 text-sm font-medium leading-5 text-right whitespace-nowrap border-b border-pale-gray">
-                    <a href="#" class="text-pale-gray hover:text-gray-hover">Edit</a>
-                  </td>
-                </tr>
-                <tr>
-                  <td class="py-4 px-6 whitespace-nowrap border-b border-pale-gray">
-                    <div class="flex items-center">
-                      <div class="flex-shrink-0 w-10 h-10">
-                        <img class="w-10 h-10 rounded-full" src="@/assets/user.png" alt="">
-                      </div>
-                      <div class="ml-4">
-                        <div class="text-sm font-medium leading-5 text-pale-gray">
-                          John Doe
-                        </div>
-                        <div class="text-sm leading-5 text-gray-500">
-                          john@example.com
-                        </div>
-                      </div>
-                    </div>
-                  </td>
-                  <td class="py-4 px-6 whitespace-nowrap border-b border-pale-gray">
-                    <div class="text-sm leading-5 text-pale-gray">
-                      Software Engineer
-                    </div>
-                    <div class="text-sm leading-5 text-gray-500">
-                      Web dev
-                    </div>
-                  </td>
-                  <td class="py-4 px-6 whitespace-nowrap border-b border-pale-gray">
-                    <span class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">Active</span>
-                  </td>
-                  <td class="py-4 px-6 text-sm leading-5 whitespace-nowrap border-b border-pale-gray text-pale-gray">
-                    Owner
-                  </td>
-                  <td class="py-4 px-6 text-sm font-medium leading-5 text-right whitespace-nowrap border-b border-pale-gray">
-                    <a href="#" class="text-pale-gray hover:text-gray-hover">
-                      Edit
-                    </a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+      <div id="user-table" class="grid mt-8 bg-pale-yellow rounded-lg w-full">
+        <div class="table-cell-string uppercase border-t border-b border-t-black border-b-black border-collapse">
+          Name
         </div>
+        <div class="table-cell-string uppercase border-t border-b border-t-black border-b-black border-collapse">
+          Birthdate
+        </div>
+        <div class="table-cell-string uppercase border-t border-b border-t-black border-b-black border-collapse">
+          Role
+        </div>
+        <div class="table-cell-string uppercase border-t border-b border-t-black border-b-black border-collapse" />
+        <template v-for="(user, i) in userRef" :key="JSON.stringify(user)">
+          <div class="py-4 px-5 flex flex-row gap-4 items-center cursor-pointer" @click="goToUsersPage(i)">
+            <img src="@/assets/user.png" alt="user" class="w-8 h-8 rounded-full">
+            <div>
+              <h1 class="font-sans text-sm font-medium text-black">
+                {{ user.username }}
+              </h1>
+              <h1 class="font-sans text-sm font-normal text-gray-500">
+                {{ user.email }}
+              </h1>
+            </div>
+          </div>
+          <h1 class="table-cell-string font-sans text-sm font-medium text-black">
+            {{ formatBirthdate(user.birthdate) }}
+          </h1>
+          <div class="py-4 px-5 flex flex-row justify-start self-start">
+            <span v-if="user.role === 0" class="inline-flex text-sm bg-red-100 text-red-500 rounded-2xl px-2">
+              Admin
+            </span>
+            <span v-else class="inline-flex text-sm bg-green-100 text-green-800 rounded-2xl px-2">
+              User
+            </span>
+          </div>
+          <div class="table-cell-string">
+            <v-mdi class="cursor-pointer" name="mdi-delete-sweep" fill="#423E41" @click="deleteUser(i)" />
+          </div>
+        </template>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import dayjs from 'dayjs'
+import { defineComponent, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+import { users } from '@/constants/devtools-users'
 
 export default defineComponent({
-  name: 'devtool-users'
+  name: 'devtool-users',
+  setup () {
+    const router = useRouter()
+
+    const userRef = ref(users)
+
+    const formatBirthdate = (birthdate: string): string => {
+      return dayjs(birthdate).format('MMMM D, YYYY')
+    }
+
+    const deleteUser = (index: number): void => {
+      if (userRef.value[index].role === 0) {
+        return
+      }
+
+      userRef.value.splice(index, 1)
+    }
+
+    const goToUsersPage = (index: number): void => {
+      router.push('/users')
+      console.log(index)
+    }
+
+    return {
+      userRef,
+      formatBirthdate,
+      deleteUser,
+      goToUsersPage
+    }
+  }
 })
 </script>
 
 <style scoped lang="scss">
+#user-table {
+  grid-template-columns: 2fr 1fr 0.5fr 0.5fr;
+}
+
 .devtool-users-page {
   @apply flex flex-row justify-center w-full min-h-screen bg-pale-gray;
 }
 
-.box-bg {
-  @apply py-3 px-6 text-xs font-medium tracking-wider leading-4 text-left uppercase border-b border-pale-gray bg-pale-yellow text-pale-gray;
+.table-cell-string {
+  @apply text-left text-sm py-4 px-5;
 }
 </style>
