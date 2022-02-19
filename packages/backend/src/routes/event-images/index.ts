96f1f17703baf22540b39f2dac758604b0cf7927
Baramee Chaiho <pahe.014700@gmail.com>
2022-02-20 04:11:22 +0700

    Create route post-eventimage

--- packages/backend/src/routes/event-images/index.ts
+++ packages/backend/src/routes/event-images/index.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify'
 
 import getEventImageRoute from './get-event-image'
+import postEventImageRoute from './post-event-image'
 
 export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promise<void> => {
   void instance.register(getEventImageRoute)
+  void instance.register(postEventImageRoute)
 }
