742f59695647457755c5f834a3cd2b8b111ff5cb
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-02-22 22:13:24 +0700

    import vue toastification

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -2,12 +2,14 @@ import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 // @ts-expect-error
 import VueMdi from 'vue-mdijs'
+import Toast from 'vue-toastification'
 
 import * as mdi from '@mdi/js'
 
 import App from './App.vue'
 import Router from './router'
 
+import 'vue-toastification/dist/index.css'
 import './globals.scss'
 
 VueMdi.add(mdi)
@@ -15,5 +17,6 @@ VueMdi.add(mdi)
 const app = createApp(App)
 app.use(Router)
 app.use(VueMdi)
+app.use(Toast)
 app.use(createPinia())
 app.mount('#app')
