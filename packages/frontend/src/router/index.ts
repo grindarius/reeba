0b053e4c5b0230db29739399fb0f6e6200d47e21
PhattharapornC <Phattharapornc62@nu.ac.th>
2022-02-24 02:16:30 +0700

    make the page scroll to top on going to new page

--- packages/frontend/src/router/index.ts
+++ packages/frontend/src/router/index.ts
@@ -107,7 +107,10 @@ const routes: Array<RouteRecordRaw> = [
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior: () => {
+    return { top: 0 }
+  }
 })
 
 export default router
