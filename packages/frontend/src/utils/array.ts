ca06c6392c0eb294bcdff2b4ceffd2ae3c3893f4
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-02-26 09:22:15 +0700

    new section generator

--- packages/frontend/src/utils/array.ts
+++ packages/frontend/src/utils/array.ts
@@ -1,3 +1,4 @@
+import { ReebAEventSeat, ReebAEventSection } from '@/types'
 import { numberToLetters } from './string'
 
 /**
@@ -18,5 +19,25 @@ export const generateEventSections = (row: number, column: number): Array<Array<
 
   return Array.from<Array<string>, Array<string>>({ length: Math.floor(row) }, (_, u) => {
     return Array.from<string, string>({ length: Math.floor(column) }, (_, v) => `${numberToLetters(u)}${v + 1}`)
+  })
+}
+
+export const actualEventSectionsGenerator = (row: number, column: number, initialSeats: Array<Array<ReebAEventSeat>>): Array<Array<ReebAEventSection>> => {
+  if (row <= 0) {
+    throw new Error('property \'row\' cannot be less than or equal to 0')
+  }
+
+  if (column <= 0) {
+    throw new Error('property \'column\' cannot be less than or equal to 0')
+  }
+
+  return Array.from<Array<ReebAEventSection>, Array<ReebAEventSection>>({ length: Math.floor(row) }, (_, u) => {
+    return Array.from<ReebAEventSection, ReebAEventSection>({ length: Math.floor(column) }, (_, v) => {
+      return {
+        rowPosition: u,
+        columnPosition: v,
+        seats: initialSeats
+      }
+    })
   })
 }
