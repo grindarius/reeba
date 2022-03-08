df86ecea024a4658c2a7faca473b519f201699ae
LzzAa35 <62424250+LzzAa35@users.noreply.github.com>
2022-03-09 03:15:11 +0700

    first results draft

--- packages/frontend/src/views/search-result.vue
+++ packages/frontend/src/views/search-result.vue
@@ -4,19 +4,19 @@
       <div class="flex flex-row">
         <div class="flex-none w-56">
           Filter
-          <div class="mt-3 divide-y overflow-y-auto pt-4 rounded text-white dark:bg-gray-800">
+          <div class="mt-3 divide-y overflow-y-auto pt-4 rounded text-pale-gray bg-pale-yellow">
             <div class="text-xl font-bold text-left pl-4 pb-2">
               Type
             </div>
-            <div class="py-4 text-xl text-white text-center font-bold dark:hover:bg-gray-700 cursor-pointer">
+            <div class="py-4 text-xl text-pale-gray text-center font-bold dark:hover:bg-yellow-hover cursor-pointer">
               Official
             </div>
 
-            <div class="py-4 text-xl text-white text-center font-bold dark:hover:bg-gray-700 cursor-pointer">
+            <div class="py-4 text-xl text-pale-gray text-center font-bold dark:hover:bg-yellow-hover cursor-pointer">
               Local
             </div>
           </div>
-          <div class="mt-3 divide-y overflow-y-auto py-4 rounded text-white dark:bg-gray-800">
+          <div class="mt-3 divide-y overflow-y-auto py-4 rounded text-pale-gray bg-pale-yellow">
             <div class="text-xl font-bold text-left pl-4 pb-2">
               Price range
             </div>
@@ -24,23 +24,26 @@
               <input type="range" class="w-full" min="1" max="4" step="1">
               <ul class="flex justify-between text-sm px-[10px]">
                 <li class="flex justify-center relative">
-                  <span class="absolute">>200</span>
+                  <span class="absolute" value="200">>200</span>
                 </li>
                 <li class="flex justify-center relative">
-                  <span class="absolute">500</span>
+                  <span class="absolute" value="500">500</span>
                 </li>
                 <li class="flex justify-center relative">
-                  <span class="absolute">1000</span>
+                  <span class="absolute" value="1000">1000</span>
                 </li>
                 <li class="flex justify-center relative">
-                  <span class="absolute">>2000</span>
+                  <span class="absolute" value="2000">>2000</span>
                 </li>
               </ul>
             </div>
           </div>
-          <div class="mt-3 divide-y overflow-y-auto py-4 rounded text-white dark:bg-gray-800">
-            <div class="text-center relative">
-              <select class="dark:bg-gray-800 rounded">
+          <div class="mt-3 divide-y overflow-y-auto py-4 rounded text-pale-gray bg-pale-yellow">
+            <div class="text-xl font-bold text-left pl-4 pb-2">
+              Date
+            </div>
+            <div class="pt-4 text-center relative">
+              <select class="bg-pale-yellow rounded">
                 <option>Choose date</option>
                 <option>Last week</option>
                 <option>Last month</option>
@@ -54,18 +57,52 @@
               </select>
             </div>
           </div>
-          <div class="mt-3 divide-y overflow-y-auto py-4 rounded text-white dark:bg-gray-800">
+          <div class="mt-3 divide-y overflow-y-auto py-4 rounded text-pale-gray bg-pale-yellow">
             <div class="text-xl font-bold text-left pl-4 pb-2">
               Type
             </div>
-            <div class="flex items-center py-2 px-3 dark:hover:bg-gray-700" v-for="(tag, i) in eventTagsSelectors" :key="`event-tag-list-checkbox-${i}`">
+            <div class="flex items-center py-2 px-3 dark:hover:bg-yellow-hover" v-for="(tag, i) in eventTagsSelectors" :key="`event-tag-list-checkbox-${i}`">
               <input :id="`event-tag-checkbox-input-${tag.tag}`" type="checkbox" class="cursor-pointer mr-10 w-4 h-4 rounded" :value="tag.tag" v-model="eventTags">
-              <label :for="`event-tag-checkbox-input-${tag.tag}`" class="cursor-pointer font-light text-white">{{ tag.name }}</label>
+              <label :for="`event-tag-checkbox-input-${tag.tag}`" class="cursor-pointer font-light text-pale-gray">{{ tag.name }}</label>
             </div>
           </div>
         </div>
         <div class="grow pl-20">
           12 repository results
+          <div class="mt-3 divide-y overflow-y-auto py-4 px-4 rounded text-pale-gray bg-pale-yellow">
+            <div class=" text-xl font-bold text-left pl-4 pb-2">
+              Name
+              <div class=" text-sm font-medium pl-3">
+                Date
+              </div>
+            </div>
+            <div class="text-base pl-3 break-all whitespace-pre-line text-ellipsis truncate">
+              Description
+              <div class="text-sm pl-4">
+                location
+              </div>
+            </div>
+          </div>
+          <div class="px-4 py-4">
+            <div class="flex items-center space-x-1">
+              <a href="#" class="flex items-center px-4 py-2 text-gray-500 bg-gray-300 rounded-md">
+                Previous
+              </a>
+
+              <a href="#" class="px-4 py-2 text-gray-700 bg-gray-200 rounded-md hover:bg-blue-400 hover:text-white">
+                1
+              </a>
+              <a href="#" class="px-4 py-2 text-gray-700 bg-gray-200 rounded-md hover:bg-blue-400 hover:text-white">
+                2
+              </a>
+              <a href="#" class="px-4 py-2 text-gray-700 bg-gray-200 rounded-md hover:bg-blue-400 hover:text-white">
+                3
+              </a>
+              <a href="#" class="px-4 py-2 font-bold text-gray-500 bg-gray-300 rounded-md hover:bg-blue-400 hover:text-white">
+                Next
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
