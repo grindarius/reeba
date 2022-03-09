e4dce47698df7ee536024176b09cd6838eb808f9
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-03-09 10:25:52 +0700

    type support and basic checks

--- packages/backend/src/routes/transactions/post-transaction.ts
+++ packages/backend/src/routes/transactions/post-transaction.ts
@@ -1,9 +1,62 @@
-import { FastifyInstance, FastifyPluginOptions } from 'fastify'
+import { FastifyInstance, FastifyPluginOptions, FastifySchema } from 'fastify'
+
+import {
+  PostTransactionRequestBody,
+  PostTransactionRequestBodySchema
+} from '@reeba/common'
+
+const schema: FastifySchema = {
+  body: PostTransactionRequestBodySchema
+}
 
 export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promise<void> => {
-  instance.post('/', async () => {
-    return {
-      hello: 'world'
+  instance.post<{ Body: PostTransactionRequestBody }>(
+    '/',
+    {
+      schema,
+      onRequest: instance.authenticate,
+      preValidation: async (request, reply) => {
+        const { eventId, datetimeId, sectionId, seatIds } = request.body
+
+        if (eventId == null || eventId === '') {
+          void reply.code(400)
+          throw new Error('body should have required property \'eventId\'')
+        }
+
+        if (datetimeId == null || datetimeId === '') {
+          void reply.code(400)
+          throw new Error('body should have required property \'datetimeId\'')
+        }
+
+        if (sectionId == null || sectionId === '') {
+          void reply.code(400)
+          throw new Error('body should have required property \'sectionId\'')
+        }
+
+        if (seatIds == null) {
+          void reply.code(400)
+          throw new Error('body should have required property \'seatIds\'')
+        }
+
+        if (!Array.isArray(seatIds)) {
+          void reply.code(400)
+          throw new Error('wrong \'seatIds\' format')
+        }
+
+        if (seatIds.length === 0) {
+          void reply.code(400)
+          throw new Error('body should have required property \'seatIds\'')
+        }
+      }
+    },
+    async (request, reply) => {
+      const { eventId, datetimeId, sectionId, seatIds } = request.body
+
+      
+
+      return {
+        hello: 'world'
+      }
     }
-  })
+  )
 }
