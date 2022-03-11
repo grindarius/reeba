1d9a95fa63ad2c99542a1b1eca22fa981807e2ac
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-03-11 22:08:40 +0700

    remove auth, add code to check taken seats

--- packages/backend/src/routes/transactions/post-transaction.ts
+++ packages/backend/src/routes/transactions/post-transaction.ts
@@ -1,6 +1,10 @@
 import { FastifyInstance, FastifyPluginOptions, FastifySchema } from 'fastify'
 
 import {
+  event_datetimes,
+  event_seats,
+  event_sections,
+  events,
   PostTransactionRequestBody,
   PostTransactionRequestBodySchema
 } from '@reeba/common'
@@ -14,7 +18,6 @@ export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promi
     '/',
     {
       schema,
-      onRequest: instance.authenticate,
       preValidation: async (request, reply) => {
         const { eventId, datetimeId, sectionId, seatIds } = request.body
 
@@ -52,7 +55,38 @@ export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promi
     async (request, reply) => {
       const { eventId, datetimeId, sectionId, seatIds } = request.body
 
-      
+      type TakenSeats = Pick<events, 'event_id'> & Pick<event_datetimes, 'event_datetime_id'> & Pick<event_sections, 'event_section_id'> & Pick<event_seats, 'event_seat_id'>
+
+      // * check if all seats are taken or not
+      // * taken seats will have seat id on both transaction_details and event_seats
+      // * available seats will have seat id on event_seats table only
+      // * this function will return an empty array if all seats are available, will return seat ids that are taken
+      const takenSeats = await instance.pg.query<TakenSeats, [events['event_id'], event_datetimes['event_datetime_id'], event_sections['event_section_id'], ...Array<event_seats['event_seat_id']>]>(
+        `select
+          events.event_id,
+          event_datetimes.event_datetime_id,
+          event_sections.event_section_id,
+          event_seats.event_seat_id
+        from "event_seats"
+        inner join "event_sections" on event_seats.event_section_id = event_sections.event_section_id
+        inner join "event_datetimes" on event_sections.event_datetime_id = event_datetimes.event_datetime_id
+        inner join "events" on event_datetimes.event_id = events.event_id
+        left join "transaction_details" on event_seats.event_seat_id = transaction_details.event_seat_id
+        where
+          transaction_details.event_seat_id is null and
+          events.event_id = $1 and
+          event_datetimes.event_datetime_id = $2 and
+          event_sections.event_section_id = $3 and
+          event_seats.event_seat_id in (${seatIds.map((_, i) => `$${i + 4}`).join(',')})`,
+        [eventId, datetimeId, sectionId, ...seatIds]
+      )
+
+      if (takenSeats.rowCount !== 0) {
+        void reply.code(400)
+        throw new Error('the following seatIds are taken: ' + takenSeats.rows.map(t => t.event_seat_id).join(', '))
+      }
+
+      // * if none is taken, register the transaction.
 
       return {
         hello: 'world'
