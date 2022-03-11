f252189cca49b2e01bc6585a6f2e96310d601ebb
LzzAa35 <62424250+LzzAa35@users.noreply.github.com>
2022-03-12 01:58:10 +0700

    adjust

--- packages/frontend/src/views/search-result.vue
+++ packages/frontend/src/views/search-result.vue
@@ -1,82 +1,70 @@
 <template>
   <div class="search-page">
     <div class="search-page-content">
-      <div class="flex flex-row">
+      <div class="hidden lg:flex flex-row">
         <div class="flex-none w-56">
-          Filter
+          <div class="mt-3 divide-y overflow-y-auto py-4 px-4 rounded text-2xl font-bold text-pale-gray bg-pale-yellow">
+            Filter
+          </div>
           <div class="mt-3 divide-y overflow-y-auto pt-4 rounded text-pale-gray bg-pale-yellow">
             <div class="text-xl font-bold text-left pl-4 pb-2">
               Type
             </div>
-            <div class="py-4 text-xl text-pale-gray text-center font-bold dark:hover:bg-yellow-hover cursor-pointer">
-              Official
-            </div>
-
-            <div class="py-4 text-xl text-pale-gray text-center font-bold dark:hover:bg-yellow-hover cursor-pointer">
-              Local
+            <div class="flex items-center py-2 px-3 dark:hover:bg-yellow-hover" v-for="(tag, i) in eventTypeSelectors" :key="`event-tag-list-checkbox-${i}`">
+              <input :id="`event-tag-checkbox-input-${tag.tag}`" type="checkbox" class="cursor-pointer mr-10 w-4 h-4 rounded" :value="tag.tag" v-model="eventTags">
+              <label :for="`event-tag-checkbox-input-${tag.tag}`" class="cursor-pointer font-medium text-pale-gray">{{ tag.name }}</label>
             </div>
           </div>
           <div class="mt-3 divide-y overflow-y-auto py-4 rounded text-pale-gray bg-pale-yellow">
             <div class="text-xl font-bold text-left pl-4 pb-2">
               Price range
             </div>
-            <div class="flex flex-col p-2 px-3 py-6 ">
-              <input type="range" class="w-full" min="1" max="4" step="1">
-              <ul class="flex justify-between text-sm px-[10px]">
-                <li class="flex justify-center relative">
-                  <span class="absolute" value="200">>200</span>
-                </li>
-                <li class="flex justify-center relative">
-                  <span class="absolute" value="500">500</span>
-                </li>
-                <li class="flex justify-center relative">
-                  <span class="absolute" value="1000">1000</span>
-                </li>
-                <li class="flex justify-center relative">
-                  <span class="absolute" value="2000">>2000</span>
-                </li>
-              </ul>
+            <div class="flex items-center py-2 px-3 dark:hover:bg-yellow-hover" v-for="(tag, i) in eventPriceRangeSelectors" :key="`event-tag-list-checkbox-${i}`">
+              <input :id="`event-tag-checkbox-input-${tag.tag}`" type="checkbox" class="cursor-pointer mr-10 w-4 h-4 rounded" :value="tag.tag" v-model="eventTags">
+              <label :for="`event-tag-checkbox-input-${tag.tag}`" class="cursor-pointer font-medium text-pale-gray">{{ tag.name }}</label>
             </div>
           </div>
           <div class="mt-3 divide-y overflow-y-auto py-4 rounded text-pale-gray bg-pale-yellow">
             <div class="text-xl font-bold text-left pl-4 pb-2">
               Date
             </div>
-            <div class="pt-4 text-center relative">
-              <select class="bg-white rounded">
-                <option>All Dates</option>
+            <!-- <div class="pt-4 text-center relative">
+              <select class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+                <option class="">
+                  All Dates
+                </option>
                 <option>Today</option>
                 <option>This Week</option>
-                <option>This Month</option>
-                <option>This Year</option>
                 <option>Next Week</option>
+                <option>This Month</option>
                 <option>Next Month</option>
-                <option>Next Year</option>
               </select>
-            </div>
+            </div> -->
           </div>
           <div class="mt-3 divide-y overflow-y-auto py-4 rounded text-pale-gray bg-pale-yellow">
             <div class="text-xl font-bold text-left pl-4 pb-2">
-              Type
+              Tag
             </div>
             <div class="flex items-center py-2 px-3 dark:hover:bg-yellow-hover" v-for="(tag, i) in eventTagsSelectors" :key="`event-tag-list-checkbox-${i}`">
               <input :id="`event-tag-checkbox-input-${tag.tag}`" type="checkbox" class="cursor-pointer mr-10 w-4 h-4 rounded" :value="tag.tag" v-model="eventTags">
-              <label :for="`event-tag-checkbox-input-${tag.tag}`" class="cursor-pointer font-light text-pale-gray">{{ tag.name }}</label>
+              <label :for="`event-tag-checkbox-input-${tag.tag}`" class="cursor-pointer font-medium text-pale-gray">{{ tag.name }}</label>
             </div>
           </div>
         </div>
-        <div class="grow pl-20">
-          12 repository results
+        <div class="grow pl-20 ">
+          <div class="mt-3 divide-y overflow-y-auto py-4 px-4 rounded text-2xl font-bold text-pale-gray bg-pale-yellow">
+            12 event results
+          </div>
           <div class="mt-3 divide-y overflow-y-auto py-4 px-4 rounded text-pale-gray bg-pale-yellow">
             <div class=" text-xl font-bold text-left pl-4 pb-2">
               Name
-              <div class=" text-sm font-medium pl-3">
+              <div class=" text-base font-medium pl-3 pt-3">
                 Date
               </div>
             </div>
-            <div class="text-base pl-3 break-all whitespace-pre-line text-ellipsis truncate">
+            <div class="text-lg pl-3 pt-3 break-all whitespace-pre-line text-ellipsis truncate">
               Description
-              <div class="text-sm pl-4">
+              <div class="text-base font-medium pl-4 pt-3">
                 location
               </div>
             </div>
@@ -112,11 +100,14 @@ import ky from 'ky'
 import { defineComponent, onMounted, Ref, ref } from 'vue'
 import { useRoute } from 'vue-router'
 
+import { useModalState } from '@/composables'
+
 export default defineComponent({
   name: 'search',
   setup () {
     const route = useRoute()
     const eventTags: Ref<Array<string>> = ref([])
+    const { state: dropdownState, toggle: toggleDropdown } = useModalState()
 
     onMounted(async () => {
       await ky('http://localhost:3000/search', {
@@ -128,6 +119,28 @@ export default defineComponent({
         }
       })
     })
+    const eventTypeSelectors: Ref<Array<{ name: string, tag: string}>> = ref([
+      { name: 'Official', tag: 'official' },
+      { name: 'Local', tag: 'local' }
+    ])
+    const eventPriceRangeSelectors: Ref<Array<{ name: string, tag: string}>> = ref([
+      { name: 'Any', tag: '9999999' },
+      { name: '< 300', tag: '300' },
+      { name: '< 600', tag: '600' },
+      { name: '< 1,200', tag: '1200' },
+      { name: '< 2,400', tag: '2400' },
+      { name: '< 4,800', tag: '4800' },
+      { name: '< 7,200', tag: '7200' },
+      { name: '< 10,000', tag: '10000' }
+    ])
+    const eventDateSelectors: Ref<Array<{ name: string, tag: string}>> = ref([
+      { name: 'All date', tag: 'alldate' },
+      { name: 'Today', tag: 'today' },
+      { name: 'This Week', tag: 'thisweek' },
+      { name: 'Next Week', tag: 'nextweek' },
+      { name: 'This Month', tag: 'thismonth' },
+      { name: 'Next Month', tag: 'nextmonth' }
+    ])
     const eventTagsSelectors: Ref<Array<{ name: string, tag: string }>> = ref([
       { name: 'Amphitheater', tag: 'amphitheater' },
       { name: 'Business', tag: 'business' },
@@ -146,8 +159,13 @@ export default defineComponent({
       { name: 'Variety', tag: 'variety' }
     ])
     return {
+      eventTypeSelectors,
+      eventPriceRangeSelectors,
       eventTagsSelectors,
-      eventTags
+      eventDateSelectors,
+      eventTags,
+      toggleDropdown,
+      dropdownState
     }
   }
 })
@@ -159,6 +177,6 @@ export default defineComponent({
 }
 
 .search-page-content {
-  @apply container mt-6;
+  @apply container mx-6 my-6;
 }
 </style>
