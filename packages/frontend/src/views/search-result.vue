a88af426c3d690cdfd773fdcfc49ffafa9e6a4a9
LzzAa35 <62424250+LzzAa35@users.noreply.github.com>
2022-03-12 22:46:42 +0700

    add mobile-menu

--- packages/frontend/src/views/search-result.vue
+++ packages/frontend/src/views/search-result.vue
@@ -1,92 +1,190 @@
 <template>
   <div class="search-page">
     <div class="search-page-content">
-      <div class="hidden lg:flex flex-row">
-        <div class="flex-none w-56">
-          <div class="mt-3 divide-y overflow-y-auto py-4 px-4 rounded text-2xl font-bold text-pale-gray bg-pale-yellow">
-            Filter
+      <div class="hidden primary-menu">
+        <div class="primary-filter">
+          <div class="primary-hade text-2xl font-bold">
+            <div class="primary-hade-text">
+              Filter
+            </div>
           </div>
-          <div class="mt-3 divide-y overflow-y-auto pt-4 rounded text-pale-gray bg-pale-yellow">
-            <div class="text-xl font-bold text-left pl-4 pb-2">
+          <div class="primary-hade">
+            <div class="primary-hade-text">
               Type
             </div>
-            <div class="flex items-center py-2 px-3 dark:hover:bg-yellow-hover" v-for="(tag, i) in eventTypeSelectors" :key="`event-tag-list-checkbox-${i}`">
-              <input :id="`event-tag-checkbox-input-${tag.tag}`" type="checkbox" class="cursor-pointer mr-10 w-4 h-4 rounded" :value="tag.tag" v-model="eventTags">
-              <label :for="`event-tag-checkbox-input-${tag.tag}`" class="cursor-pointer font-medium text-pale-gray">{{ tag.name }}</label>
+            <div class="flex items-center py-2 px-3 dark:hover:bg-yellow-hover" v-for="(tag, i) in eventTypeSelectors" :key="`event-type-list-checkbox-${i}`">
+              <input :id="`event-tag-checkbox-input-${tag.tag}`" type="checkbox" class="cursor-pointer mr-10 w-4 h-4 rounded accent-pink-500" :value="tag.tag" v-model="eventTags">
+              <label :for="`event-tag-checkbox-input-${tag.tag}`" class="cursor-pointer font-medium text-black">{{ tag.name }}</label>
             </div>
           </div>
-          <div class="mt-3 divide-y overflow-y-auto py-4 rounded text-pale-gray bg-pale-yellow">
-            <div class="text-xl font-bold text-left pl-4 pb-2">
+          <div class="primary-hade">
+            <div class="primary-hade-text">
               Price range
             </div>
-            <div class="flex items-center py-2 px-3 dark:hover:bg-yellow-hover" v-for="(tag, i) in eventPriceRangeSelectors" :key="`event-tag-list-checkbox-${i}`">
-              <input :id="`event-tag-checkbox-input-${tag.tag}`" type="checkbox" class="cursor-pointer mr-10 w-4 h-4 rounded" :value="tag.tag" v-model="eventTags">
-              <label :for="`event-tag-checkbox-input-${tag.tag}`" class="cursor-pointer font-medium text-pale-gray">{{ tag.name }}</label>
+            <div class="flex items-center py-2 px-3 dark:hover:bg-yellow-hover" v-for="(tag, i) in eventPriceRangeSelectors" :key="`event-price-range-list-checkbox-${i}`">
+              <input :id="`event-tag-checkbox-input-${tag.tag}`" type="checkbox" class="cursor-pointer mr-10 w-4 h-4 rounded accent-pink-500" :value="tag.tag" v-model="eventTags">
+              <label :for="`event-tag-checkbox-input-${tag.tag}`" class="cursor-pointer font-medium text-black">{{ tag.name }}</label>
             </div>
           </div>
-          <div class="mt-3 divide-y overflow-y-auto py-4 rounded text-pale-gray bg-pale-yellow">
-            <div class="text-xl font-bold text-left pl-4 pb-2">
+          <div class="mt-3 divide-y py-4 rounded text-black bg-pale-yellow">
+            <div class="primary-hade-text">
               Date
             </div>
-            <!-- <div class="pt-4 text-center relative">
-              <select class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-                <option class="">
-                  All Dates
-                </option>
-                <option>Today</option>
-                <option>This Week</option>
-                <option>Next Week</option>
-                <option>This Month</option>
-                <option>Next Month</option>
-              </select>
-            </div> -->
+            <div>
+              <r-dropdown :values="reformattedArrayeventDateSelectors" v-model:selected="eventDateSelectors" style="padding: 2%;" />
+            </div>
           </div>
-          <div class="mt-3 divide-y overflow-y-auto py-4 rounded text-pale-gray bg-pale-yellow">
-            <div class="text-xl font-bold text-left pl-4 pb-2">
+          <div class="primary-hade">
+            <div class="primary-hade-text">
               Tag
             </div>
             <div class="flex items-center py-2 px-3 dark:hover:bg-yellow-hover" v-for="(tag, i) in eventTagsSelectors" :key="`event-tag-list-checkbox-${i}`">
-              <input :id="`event-tag-checkbox-input-${tag.tag}`" type="checkbox" class="cursor-pointer mr-10 w-4 h-4 rounded" :value="tag.tag" v-model="eventTags">
-              <label :for="`event-tag-checkbox-input-${tag.tag}`" class="cursor-pointer font-medium text-pale-gray">{{ tag.name }}</label>
+              <input :id="`event-tag-checkbox-input-${tag.tag}`" type="checkbox" class="cursor-pointer mr-10 w-4 h-4 rounded accent-pink-500" :value="tag.tag" v-model="eventTags">
+              <label :for="`event-tag-checkbox-input-${tag.tag}`" class="cursor-pointer font-medium text-black">{{ tag.name }}</label>
             </div>
           </div>
         </div>
-        <div class="grow pl-20 ">
-          <div class="mt-3 divide-y overflow-y-auto py-4 px-4 rounded text-2xl font-bold text-pale-gray bg-pale-yellow">
+        <div class="grow pl-20">
+          <div class="mt-3 divide-y py-4 rounded text-black bg-pale-yellow text-2xl font-bold overflow-y-auto px-5 ">
             12 event results
           </div>
-          <div class="mt-3 divide-y overflow-y-auto py-4 px-4 rounded text-pale-gray bg-pale-yellow">
-            <div class=" text-xl font-bold text-left pl-4 pb-2">
-              Name
-              <div class=" text-base font-medium pl-3 pt-3">
-                Date
+          <div class="mt-3 divide-y py-4 rounded text-black bg-pale-yellow overflow-y-auto px-5 list-inside">
+            <div class="text-xl font-bold text-left pb-2">
+              <div class="">
+                <v-mdi name="mdi-ticket-outline" fill="#666265" />
+                Name
+              </div>
+              <div class=" text-base font-medium pt-3">
+                <li class="px-3">
+                  Date
+                </li>
               </div>
             </div>
-            <div class="text-lg pl-3 pt-3 break-all whitespace-pre-line text-ellipsis truncate">
+            <div class="text-lg font-medium pt-3 break-all whitespace-pre-line text-ellipsis truncate">
               Description
-              <div class="text-base font-medium pl-4 pt-3">
-                location
+              <div class="text-base font-medium pt-3">
+                <li class="px-3">
+                  location
+                </li>
               </div>
             </div>
           </div>
           <div class="px-4 py-4">
-            <div class="flex items-center space-x-1">
-              <a href="#" class="flex items-center px-4 py-2 text-gray-500 bg-gray-300 rounded-md">
-                Previous
-              </a>
+            <div class="flex flex-col items-center">
+              <span class="text-sm text-white">
+                Showing
+                <span class="font-semibold text-white" />
+                <span class="font-semibold text-white">
+                  10
+                </span>
+                of
+                <span class="font-semibold text-white">
+                  100
+                </span> Entries
+              </span>
 
-              <a href="#" class="px-4 py-2 text-gray-700 bg-gray-200 rounded-md hover:bg-blue-400 hover:text-white">
-                1
-              </a>
-              <a href="#" class="px-4 py-2 text-gray-700 bg-gray-200 rounded-md hover:bg-blue-400 hover:text-white">
-                2
-              </a>
-              <a href="#" class="px-4 py-2 text-gray-700 bg-gray-200 rounded-md hover:bg-blue-400 hover:text-white">
-                3
-              </a>
-              <a href="#" class="px-4 py-2 font-bold text-gray-500 bg-gray-300 rounded-md hover:bg-blue-400 hover:text-white">
-                Next
-              </a>
+              <div class="inline-flex mt-2 xs:mt-0">
+                <button class="py-2 px-4 text-sm font-medium text-back bg-pale-yellow hover:bg-yellow-hover rounded-l">
+                  Prev
+                </button>
+                <button class="py-2 px-4 text-sm font-medium text-back bg-pale-yellow hover:bg-yellow-hover rounded-r border-0 border-l">
+                  Next
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="lg:hidden mobile-menu">
+        <div class="mobile-filter-box overflow-y-auto">
+          <div class="text-xl font-bold text-left pl-4">
+            Type
+          </div>
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-y-1 gap-x-2 px-2 py-2 mt-2 ">
+            <div class="flex items-center px-2 py-2 dark:hover:bg-yellow-hover rounded" v-for="(tag, i) in eventTypeSelectors" :key="`event-tag-list-checkbox-${i}`">
+              <input :id="`event-tag-checkbox-input-${tag.tag}`" type="checkbox" class="mr-10 w-4 h-4 rounded border border-gray-300 focus:border-gray-600 accent-pink-500 focus:ring-3" :value="tag.tag" v-model="eventTags">
+              <label :for="`event-tag-checkbox-input-${tag.tag}`" class="font-medium text-black">{{ tag.name }}</label>
+            </div>
+          </div>
+        </div>
+        <div class="mobile-filter-box overflow-y-auto">
+          <div class="text-xl font-bold text-left pl-4">
+            Price range
+          </div>
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-y-1 gap-x-2 px-2 py-2 mt-2">
+            <div class="flex items-center px-2 py-2 dark:hover:bg-yellow-hover rounded" v-for="(tag, i) in eventPriceRangeSelectors" :key="`event-tag-list-checkbox-${i}`">
+              <input :id="`event-tag-checkbox-input-${tag.tag}`" type="checkbox" class="mr-10 w-4 h-4 rounded border border-gray-300 focus:border-gray-600 accent-pink-500 focus:ring-3" :value="tag.tag" v-model="eventTags">
+              <label :for="`event-tag-checkbox-input-${tag.tag}`" class="font-medium text-black">{{ tag.name }}</label>
+            </div>
+          </div>
+        </div>
+        <div class="mobile-filter-box">
+          <div class="text-xl font-bold text-left pl-4">
+            Date
+          </div>
+          <div>
+            <r-dropdown :values="reformattedArrayeventDateSelectors" v-model:selected-name="eventDateSelectors" style="padding: 2%;" />
+          </div>
+        </div>
+        <div class="mobile-filter-box overflow-y-auto">
+          <div class="text-xl font-bold text-left pl-4">
+            Tag
+          </div>
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-y-1 gap-x-2 px-2 py-2 mt-2">
+            <div class="flex items-center px-2 py-2 dark:hover:bg-yellow-hover rounded" v-for="(tag, i) in eventTagsSelectors" :key="`event-tag-list-checkbox-${i}`">
+              <input :id="`event-tag-checkbox-input-${tag.tag}`" type="checkbox" class="mr-10 w-4 h-4 rounded border border-gray-300 focus:border-gray-600 accent-pink-500 focus:ring-3" :value="tag.tag" v-model="eventTags">
+              <label :for="`event-tag-checkbox-input-${tag.tag}`" class="font-medium text-black">{{ tag.name }}</label>
+            </div>
+          </div>
+        </div>
+        <div class="grow">
+          <div class="mt-3 divide-y py-4 rounded text-black bg-pale-yellow px-4 text-2xl font-bold overflow-y-auto">
+            12 event results
+          </div>
+          <div class="mt-3 divide-y py-4 rounded text-black bg-pale-yellow overflow-y-auto px-5 list-inside">
+            <div class="text-xl font-bold text-left pb-2">
+              <div class="">
+                <v-mdi name="mdi-ticket-outline" fill="#666265" />
+                Name
+              </div>
+              <div class=" text-base font-medium pt-3">
+                <li class="px-3">
+                  Date
+                </li>
+              </div>
+            </div>
+            <div class="text-lg font-medium pt-3 break-all whitespace-pre-line text-ellipsis truncate">
+              Description
+              <div class="text-base font-medium pt-3">
+                <li class="px-3">
+                  location
+                </li>
+              </div>
+            </div>
+          </div>
+          <div class="px-4 py-4">
+            <div class="flex flex-col items-center">
+              <span class="text-sm text-white">
+                Showing
+                <span class="font-semibold text-white" />
+                <span class="font-semibold text-white">
+                  10
+                </span>
+                of
+                <span class="font-semibold text-white">
+                  100
+                </span> Entries
+              </span>
+
+              <div class="inline-flex mt-2 xs:mt-0">
+                <button class="py-2 px-4 text-sm font-medium text-back bg-pale-yellow hover:bg-yellow-hover rounded-l">
+                  Prev
+                </button>
+                <button class="py-2 px-4 text-sm font-medium text-back bg-pale-yellow hover:bg-yellow-hover rounded-r border-0 border-l">
+                  Next
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -100,10 +198,14 @@ import ky from 'ky'
 import { defineComponent, onMounted, Ref, ref } from 'vue'
 import { useRoute } from 'vue-router'
 
+import RDropdown from '@/components/r-dropdown.vue'
 import { useModalState } from '@/composables'
 
 export default defineComponent({
   name: 'search',
+  components: {
+    'r-dropdown': RDropdown
+  },
   setup () {
     const route = useRoute()
     const eventTags: Ref<Array<string>> = ref([])
@@ -141,7 +243,8 @@ export default defineComponent({
       { name: 'This Month', tag: 'thismonth' },
       { name: 'Next Month', tag: 'nextmonth' }
     ])
-    const eventTagsSelectors: Ref<Array<{ name: string, tag: string }>> = ref([
+    const reformattedArrayeventDateSelectors = eventDateSelectors.value.map(name => name.name)
+    const eventTagsSelectors: Ref<Array<{ name: string, tag: string}>> = ref([
       { name: 'Amphitheater', tag: 'amphitheater' },
       { name: 'Business', tag: 'business' },
       { name: 'Concert', tag: 'concert' },
@@ -165,7 +268,8 @@ export default defineComponent({
       eventDateSelectors,
       eventTags,
       toggleDropdown,
-      dropdownState
+      dropdownState,
+      reformattedArrayeventDateSelectors
     }
   }
 })
@@ -178,5 +282,29 @@ export default defineComponent({
 
 .search-page-content {
   @apply container mx-6 my-6;
+}
+
+.primary-menu {
+  @apply lg:flex flex-row;
+}
+
+.primary-filter {
+  @apply flex-none w-56;
+}
+
+.primary-hade {
+  @apply mt-3 divide-y overflow-y-auto py-4 rounded text-black bg-pale-yellow;
+}
+
+.primary-hade-text {
+  @apply text-xl font-bold text-left pl-4 pb-2;
+}
+
+.mobile-menu {
+  @apply items-center grow px-2;
+}
+
+.mobile-filter-box {
+  @apply mt-3 divide-y pt-4 rounded text-black bg-pale-yellow;
 }
 </style>
