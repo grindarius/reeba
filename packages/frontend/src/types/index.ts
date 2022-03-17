653299b0cb2b70eaf46e6b5229ee4661a2818ab4
Baramee Chaiho <pahe.014700@gmail.com>
2022-03-17 22:31:11 +0700

    Created enum EVENT_PRICE

--- packages/frontend/src/types/index.ts
+++ packages/frontend/src/types/index.ts
@@ -61,3 +61,21 @@ export interface TransactionStore {
   datetimeId: string
   section: TransactionStoreSection
 }
+
+export enum EVENT_PRICE {
+  ANY = 'ANY',
+  // <300
+  PRICE_LEVEL_1 = '<300',
+  // <600
+  PRICE_LEVEL_2 = '<600',
+  // <1,200
+  PRICE_LEVEL_3 = '<1200',
+  // <2,400
+  PRICE_LEVEL_4 = '<2400',
+  // <4,800
+  PRICE_LEVEL_5 = '<4800',
+  // <7,200
+  PRICE_LEVEL_6 = '<7200',
+  // <10,000
+  PRICE_LEVEL_7 = '<10000'
+}
