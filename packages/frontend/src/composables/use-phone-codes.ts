15290dacead78a14d43b6f62b26be13d87ea4abf
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-03-19 12:33:59 +0700

    separate phone code to different component

---
+++ packages/frontend/src/composables/use-phone-codes.ts
@@ -0,0 +1,45 @@
+import { countries } from 'countries-list'
+import { computed, ComputedRef, Ref, ref } from 'vue'
+
+import { CountryCode } from '@/types'
+
+export interface UsePhoneCodes {
+  selectedPhoneCountryCode: Ref<CountryCode>
+  phoneCodesList: ComputedRef<Array<CountryCode>>
+  onPhoneCountryCodeClicked: (index: number) => void
+  countryCodeString: (code: CountryCode) => string
+}
+
+export const usePhoneCodes = (defaultCountryCode: CountryCode = { name: '', phoneCode: '' }): UsePhoneCodes => {
+  const selectedPhoneCountryCode = ref(defaultCountryCode)
+
+  const onPhoneCountryCodeClicked = (index: number): void => {
+    selectedPhoneCountryCode.value = phoneCodesList.value[index]
+  }
+
+  const countryCodeString = (code: CountryCode): string => {
+    return `${code.name} (+${code.phoneCode})`
+  }
+
+  const phoneCodesList = computed(() => {
+    return Object.values(countries).flatMap(ct => {
+      const phoneCodeArray = ct.phone.split(',')
+
+      return phoneCodeArray.map(code => {
+        const ret: CountryCode = {
+          name: ct.name,
+          phoneCode: code
+        }
+
+        return ret
+      })
+    })
+  })
+
+  return {
+    selectedPhoneCountryCode,
+    phoneCodesList,
+    onPhoneCountryCodeClicked,
+    countryCodeString
+  }
+}
