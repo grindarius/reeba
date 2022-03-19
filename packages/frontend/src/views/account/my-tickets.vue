7fba0d02f1a32ee99a500b0fdcc2bbf1d57e7ae1
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-03-19 13:46:45 +0700

    change my tickets page to daisyui

--- packages/frontend/src/views/account/my-tickets.vue
+++ packages/frontend/src/views/account/my-tickets.vue
@@ -19,9 +19,7 @@
           <h2>Thursday, 7 April 2022</h2>
           <div class="flex flex-col">
             <div class="flex justify-center">
-              <button class="command-buttons" @click="open">
-                Transfer
-              </button>
+              <label for="transfer-ownership-modal" class="btn modal-button">Transfer Ownership</label>
               <router-link to="/select-seat" class="command-buttons">
                 Change descriptions
               </router-link>
@@ -33,51 +31,24 @@
         </div>
       </div>
     </section>
-    <div class="cursor-pointer modal-background" v-if="state">
-      <div class="modal">
-        <div class="modal-content">
-          <div class="modal-title">
-            <h1>Transfer ownership</h1>
-            <button class="modal-close-button-icon" @click="close">
-              <v-mdi fill="#fff" name="mdi-close-circle-outline" />
-            </button>
-          </div>
-          <div class="modal-body">
-            <h1 class="font-semibold text-white">
-              Who are you going to transfer the ticket to?
-            </h1>
-            <div class="searchbar">
-              <label>
-                <input type="text" name="searchbar" placeholder="Search">
-              </label>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <div>
-              <button class="modal-close-button" type="button" @click="close">
-                Close
-              </button>
-              <button class="modal-accept-button" type="button" @click="close">
-                Submit changes
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="modal-background-color" v-if="state" />
   </div>
+  <input type="checkbox" id="transfer-ownership-modal" class="modal-toggle">
+  <label for="transfer-ownership-modal" class="modal cursor-pointer" style="background-color: #00000055;">
+    <label class="modal-box relative" for="">
+      <h3 class="text-lg font-bold">Congratulations random Interner user!</h3>
+      <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+    </label>
+  </label>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted } from 'vue'
 
-import { useCounter, useModalState } from '@/composables'
+import { useCounter } from '@/composables'
 
 export default defineComponent({
   name: 'my-tickets',
   setup () {
-    const { state, open, close } = useModalState()
     const { life } = useCounter()
 
     onMounted(() => {
@@ -85,9 +56,7 @@ export default defineComponent({
     })
 
     return {
-      state,
-      close,
-      open
+      life
     }
   }
 })
@@ -122,25 +91,6 @@ export default defineComponent({
   }
 }
 
-.searchbar {
-  @apply flex flex-col justify-center;
-
-  & input {
-    @apply pr-2 pl-11 w-96 h-7 text-black bg-white rounded-lg shadow-lg outline-none;
-  }
-}
-
-label {
-  @apply relative;
-
-  &:before {
-    @apply absolute bottom-0 left-3 w-5;
-    top: 3px;
-    content: '';
-    background: url('data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22%20version%3D%221.1%22%20id%3D%22mdi-magnify%22%20width%3D%2224%22%20height%3D%2224%22%20viewBox%3D%220%200%2024%2024%22%3E%3Cpath%20fill%3D%22%23D5A755%22%20d%3D%22M9.5%2C3A6.5%2C6.5%200%200%2C1%2016%2C9.5C16%2C11.11%2015.41%2C12.59%2014.44%2C13.73L14.71%2C14H15.5L20.5%2C19L19%2C20.5L14%2C15.5V14.71L13.73%2C14.44C12.59%2C15.41%2011.11%2C16%209.5%2C16A6.5%2C6.5%200%200%2C1%203%2C9.5A6.5%2C6.5%200%200%2C1%209.5%2C3M9.5%2C5C7%2C5%205%2C7%205%2C9.5C5%2C12%207%2C14%209.5%2C14C12%2C14%2014%2C12%2014%2C9.5C14%2C7%2012%2C5%209.5%2C5Z%22%2F%3E%3C%2Fsvg%3E');
-  }
-}
-
 .page-header {
   @apply text-4xl font-semibold text-white;
 }
@@ -149,47 +99,4 @@ label {
   @apply py-2 px-3 mr-1 text-xs font-bold text-white rounded-full bg-pale-gray hover:bg-gray-hover;
 }
 
-.modal-background {
-  @apply flex overflow-y-auto overflow-x-hidden fixed inset-0 z-50 justify-center items-center outline-none focus:outline-none;
-}
-
-.modal-background-color {
-  @apply fixed inset-0 top-0 z-40 bg-black opacity-25;
-}
-
-.modal {
-  @apply relative my-6 mx-auto w-full max-w-xl;
-}
-
-.modal-content {
-  @apply flex relative flex-col w-full rounded-lg border-0 shadow-lg outline-none focus:outline-none bg-pale-gray;
-}
-
-.modal-title {
-  @apply flex justify-between items-start p-5 rounded-t;
-
-  & h1 {
-    @apply text-3xl font-semibold text-white;
-  }
-}
-
-.modal-close-button-icon {
-  @apply float-right p-1 ml-auto text-3xl font-semibold leading-none text-black border-0 outline-none focus:outline-none;
-}
-
-.modal-body {
-  @apply flex relative flex-col gap-y-6 justify-center items-center py-3 px-5;
-}
-
-.modal-footer {
-  @apply flex justify-end items-center p-6 rounded-b;
-}
-
-.modal-close-button {
-  @apply py-2 px-6 mr-1 mb-1 text-lg font-bold uppercase rounded-lg transition-all duration-150 ease-linear outline-none focus:outline-none text-pale-gray bg-pale-yellow;
-}
-
-.modal-accept-button {
-  @apply py-2 px-6 mr-1 mb-1 text-lg font-bold uppercase rounded-lg transition-all duration-150 ease-linear outline-none focus:outline-none text-pale-gray bg-pale-yellow;
-}
 </style>
