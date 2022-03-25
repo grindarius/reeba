7e6079045338b4dcc37af799ade0d1048e15650a
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-03-26 05:14:27 +0700

    moving follow functions to its own route

--- packages/backend/src/routes/followings/post-unfollow.ts
+++ packages/backend/src/routes/followings/post-unfollow.ts
@@ -6,13 +6,10 @@ import {
   PostUnfollowReplySchema,
   PostUnfollowRequestBody,
   PostUnfollowRequestBodySchema,
-  PostUnfollowRequestParams,
-  PostUnfollowRequestParamsSchema,
   user_followers
 } from '@reeba/common'
 
 const schema: FastifySchema = {
-  params: PostUnfollowRequestParamsSchema,
   body: PostUnfollowRequestBodySchema,
   response: {
     200: PostUnfollowReplySchema,
@@ -21,32 +18,26 @@ const schema: FastifySchema = {
 }
 
 export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promise<void> => {
-  instance.post<{ Params: PostUnfollowRequestParams, Body: PostUnfollowRequestBody, Reply: PostUnfollowReply }>(
-    '/:username/unfollow',
+  instance.post<{ Body: PostUnfollowRequestBody, Reply: PostUnfollowReply }>(
+    '/unfollow',
     {
       schema,
+      onRequest: instance.authenticate,
       preValidation: async (request, reply) => {
-        const { username } = request.params
         const { usernameToUnfollow } = request.body
-
-        if (username == null || username === '') {
-          void reply.code(400)
-          throw new Error('params should have required property \'username\'')
-        }
 
         if (usernameToUnfollow == null || usernameToUnfollow === '') {
           void reply.code(400)
-          throw new Error('body should have required property \'usernameToFollow\'')
+          throw new Error('body should have required property \'usernameToUnfollow\'')
         }
       }
     },
     async (request) => {
-      const { username } = request.params
       const { usernameToUnfollow } = request.body
 
-      await instance.pg.query<user_followers, [user_followers['following_user_id'], user_followers['followed_user_id']]>(
-        'delete from user_followers where following_user_id = $1 and followed_user_id = $2',
-        [username, usernameToUnfollow]
+      await instance.pg.query<user_followers, [user_followers['following_username'], user_followers['followed_username']]>(
+        'delete from "user_followers" where following_username = $1 and followed_username = $2',
+        [request.user.username, usernameToUnfollow]
       )
 
       return {
--- packages/backend/src/routes/users/index.ts
+++ packages/backend/src/routes/users/index.ts
@@ -2,12 +2,8 @@ import { FastifyInstance, FastifyPluginOptions } from 'fastify'
 
 import getUserRoute from './get-user'
 import getUserRelatedEventsRoute from './get-user-related-events'
-import postFollowRoute from './post-follow'
-import postUnfollowRoute from './post-unfollow'
 
 export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promise<void> => {
   void instance.register(getUserRoute)
   void instance.register(getUserRelatedEventsRoute)
-  void instance.register(postFollowRoute)
-  void instance.register(postUnfollowRoute)
 }
