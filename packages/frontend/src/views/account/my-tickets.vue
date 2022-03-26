f92a8aaa2e720baf5400d20bffe7442bc801253b
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-03-26 19:28:40 +0700

    apply api call to my tickets page

--- packages/frontend/src/views/account/my-tickets.vue
+++ packages/frontend/src/views/account/my-tickets.vue
@@ -4,65 +4,36 @@
       My tickets
     </h2>
     <section class="mt-8">
-      <!--<div class="content-box-inside">
-        <div class="content-box-setting-image">
-          <img class="content-box-picture" :src="`${getEventImage({ eventId: $route.params.eventId as string ?? '' }).url}`">
-        </div>
-        <div class="hero">
-          <div class="hero-content">
-            <div class="content-box-textbox">
-              <h1>BTS</h1>
-              <h3>Seat</h3>
-              <h2>C4-C09</h2>
-              <h3>Prices</h3>
-              <h2>6800 THB</h2>
-              <h3>Show date</h3>
-              <h2>Wednesday, 6 April 2022</h2>
-              <h2>Thursday, 7 April 2022</h2>
-              <div class="flex flex-col">
-                <div class="flex justify-center">
-                  <label for="transfer-ownership-modal" class="btn modal-button">Transfer Ownership</label>
-                  <router-link to="/select-seat" class="command-buttons">
-                    Change descriptions
-                  </router-link>
-                  <router-link to="/receipt" class="command-buttons">
-                    View receipt
-                  </router-link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>-->
-      <div class="hero">
+      <div class="hero" v-for="e in eventsList.events" :key="`my-ticket-event-list-${e.id}`">
         <div class="hero-content flex-col lg:flex-row">
           <div class="content-box-setting-image">
-            <img class="content-box-picture rounded-lg shadow-2xl" :src="`${getEventImage({ eventId: $route.params.eventId as string ?? '' }).url}`">
+            <img class="content-box-picture rounded-lg shadow-2xl" :src="`${getEventImage({ eventId: e.id}).url}`">
           </div>
           <div class="content-box-textbox">
-            <h1>BTS</h1>
+            <h1>{{ e.name }}</h1>
             <h3>Seat</h3>
-            <h2>C4-C09</h2>
+            <h2>{{ e.section.name }}</h2>
             <h3>Prices</h3>
-            <h2>6800 THB</h2>
+            <h2>{{ e.totalPrice }} THB</h2>
             <h3>Show date</h3>
-            <h2>Wednesday, 6 April 2022</h2>
-            <h2>Thursday, 7 April 2022</h2>
+            <h2 class="mb-2">
+              {{ formatTimeString(e.time.start) }}
+            </h2>
 
-            <div class="btn-group">
-              <button class="btn btn-active">
-                <label for="transfer-ownership-modal" class="btn modal-button">Transfer Ownership</label>
+            <div class="flex flex-col lg:flex-row space-y-1 space-x-0 lg:space-y-0 lg:space-x-1">
+              <button class="btn">
+                <label for="transfer-ownership-modal" class="modal-button rounded-r-none">Transfer Ownership</label>
               </button>
-              <button class="btn btn-active">
-                <router-link to="/select-seat" class="command-buttons">
-                  Change descriptions
-                </router-link>
-              </button>
-              <button class="btn btn-active">
-                <router-link to="/receipt" class="command-buttons">
+              <router-link to="/select-seat" custom v-slot="{ navigate }">
+                <button class="btn" @click="navigate">
+                  Change seats
+                </button>
+              </router-link>
+              <router-link to="/receipt" custom v-slot="{ navigate }">
+                <button class="btn" @click="navigate">
                   View receipt
-                </router-link>
-              </button>
+                </button>
+              </router-link>
             </div>
           </div>
         </div>
@@ -81,19 +52,21 @@
 
 <script lang="ts">
 import ky from 'ky'
-import { defineComponent, onMounted } from 'vue'
+import { defineComponent, onMounted, Ref, ref } from 'vue'
 
 import { GetMyTicketsReply } from '@reeba/common'
 
 import { getEventImage, getMyTickets } from '@/api/endpoints'
 import { useCounter } from '@/composables'
 import { useAuthStore } from '@/store/use-auth-store'
+import { formatTimeString } from '@/utils'
 
 export default defineComponent({
   name: 'my-tickets',
   setup () {
     const { life } = useCounter()
     const store = useAuthStore()
+    const eventsList:Ref<GetMyTicketsReply> = ref({ events: [] })
 
     onMounted(async () => {
       console.log(life.value)
@@ -105,20 +78,22 @@ export default defineComponent({
           Authorization: `Bearer ${store.userData.token}`
         }
       }).json<GetMyTicketsReply>()
-      console.log(response)
+      eventsList.value.events = response.events ?? []
     })
 
     return {
       life,
-      getEventImage
+      getEventImage,
+      eventsList,
+      formatTimeString
     }
   }
 })
 </script>
 
 <style scoped lang="scss">
-.hero,  {
-  @apply flex flex-none flex-col rounded-lg md:flex-row bg-pale-yellow;
+.hero  {
+  @apply flex flex-col rounded-lg md:flex-row bg-base-200;
 }
 
 .content-box-picture {
@@ -133,24 +108,20 @@ export default defineComponent({
   @apply p-6 space-y-2 w-full text-left md:p-2 md:w-4/5;
 
   & h1 {
-    @apply text-4xl font-bold text-gray-900;
+    @apply text-4xl font-bold text-white;
   }
 
   & h2 {
-    @apply text-xs font-normal text-gray-600;
+    @apply text-xs font-normal text-white;
   }
 
   & h3 {
-    @apply text-xs font-normal text-gray-900;
+    @apply text-base font-bold text-white ;
   }
 }
 
 .page-header {
   @apply text-4xl font-semibold text-white;
-}
-
-.command-buttons {
-  @apply py-2 px-3 mr-1 text-xs font-bold text-white rounded-lg bg-pale-gray hover:bg-gray-hover;
 }
 
 </style>
