0a4c6c70316eaeaec3520b136c1801da21521815
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-03-27 18:11:05 +0700

    rename links

--- packages/frontend/src/router/index.ts
+++ packages/frontend/src/router/index.ts
@@ -19,11 +19,6 @@ const routes: Array<RouteRecordRaw> = [
     component: Page.Signin
   },
   {
-    path: '/payment',
-    name: 'Payment',
-    component: Page.Payment
-  },
-  {
     path: '/account',
     name: 'Account',
     component: Page.Account,
@@ -102,6 +97,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/:username/:eventId/:datetimeId',
     name: 'Select Seat',
     component: Page.SelectSeat
+  },
+  {
+    path: '/:username/:eventId/:datetimeId/payment',
+    name: 'Payment',
+    component: Page.Payment
   },
   {
     path: '/:pathMatch(.*)*',
