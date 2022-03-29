c523417ae094be1ec1d70c82dcc209642539083a
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-03-29 16:29:26 +0700

    api calls for devtools users page

--- packages/frontend/src/views/account/devtools/devtool-users.vue
+++ packages/frontend/src/views/account/devtools/devtool-users.vue
@@ -1,88 +1,168 @@
 <template>
   <div class="devtool-users-page">
     <div class="container">
-      <h1 class="page-header">
-        Users
-      </h1>
-      <div class="user-table">
-        <div class="uppercase border-t border-b border-collapse table-cell-string border-t-black border-b-black">
-          Name
+      <div class="flex flex-row justify-between mb-4">
+        <h1 class="page-header">
+          {{ userData.total }} users
+        </h1>
+        <div class="flex flex-row gap-3">
+          <router-link custom :to="{ name: 'Developer Users', query: { page: page - 1 } }" v-slot="{ navigate }">
+            <button class="btn btn-circle btn-outline" :disabled="page - 1 === 0" @click="navigate">
+              <v-mdi name="mdi-arrow-left-thin" fill="#D5A755" />
+            </button>
+          </router-link>
+          <router-link custom :to="{ name: 'Developer Users', query: { page: page + 1} }" v-slot="{ navigate }">
+            <button class="btn btn-circle btn-outline" :disabled="(page * 30) > userData.total" @click="navigate">
+              <v-mdi name="mdi-arrow-right-thin" fill="#D5A755" />
+            </button>
+          </router-link>
+          <select class="select select-ghost max-w-xs" v-model="sortOptions">
+            <option value="name-asc">
+              <h1 class="font-bold">
+                Sort by
+              </h1> name ↑
+            </option>
+            <option value="name-desc">
+              <h1 class="font-bold">
+                Sort by
+              </h1> name ↓
+            </option>
+            <option value="regis-asc">
+              <h1 class="font-bold">
+                Sort by
+              </h1> registration date ↑
+            </option>
+            <option value="regis-desc">
+              <h1 class="font-bold">
+                Sort by
+              </h1> registration date ↓
+            </option>
+          </select>
         </div>
-        <div class="uppercase border-t border-b border-collapse table-cell-string border-t-black border-b-black">
-          Birthdate
+      </div>
+      <div class="block lg:hidden">
+        small
+      </div>
+      <div class="hidden lg:block">
+        <div class="overflow-x-auto w-full">
+          <table class="table w-full">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Registration date</th>
+                <th>Is admin</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="user in userData.users" :key="user.username">
+                <td>
+                  <div class="flex items-center space-x-3">
+                    <div class="avatar">
+                      <div class="mask mask-squircle w-12 h-12">
+                        <img :src="`${getUserAvatar({ username: user.username }).url}`" :alt="user.username">
+                      </div>
+                    </div>
+                    <div>
+                      <div class="font-bold">
+                        {{ user.username }}
+                        <v-mdi v-if="user.isAdmin ? false : user.isVerified" name="mdi-check-decagram" fill="#D5A755" title="Verified" />
+                        <v-mdi v-if="user.isAdmin" name="mdi-crown" title="Admin" size="30" fill="#D5A755" />
+                      </div>
+                      <div class="text-sm opacity-50">
+                        {{ user.email }}
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  {{ formatTimeString(user.registrationDatetime, 'MMMM D, YYYY HH:mm:ss') }}
+                </td>
+                <td>{{ user.isAdmin }}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        <div class="uppercase border-t border-b border-collapse table-cell-string border-t-black border-b-black">
-          Role
-        </div>
-        <div class="uppercase border-t border-b border-collapse table-cell-string border-t-black border-b-black" />
-        <template v-for="(user, i) in userRef" :key="JSON.stringify(user)">
-          <div class="flex flex-row gap-4 items-center py-4 px-5 cursor-pointer" @click="goToUsersPage(i)">
-            <img src="@/assets/user.png" :alt="user.username" class="w-8 h-8 rounded-full">
-            <div>
-              <h1 class="font-sans text-sm font-medium text-black">
-                {{ user.username }}
-              </h1>
-              <h1 class="font-sans text-sm font-normal text-gray-500">
-                {{ user.email }}
-              </h1>
-            </div>
-          </div>
-          <h1 class="font-mono text-sm font-medium text-black table-cell-string">
-            {{ formatBirthdate(user.birthdate) }}
-          </h1>
-          <div class="flex flex-row justify-start self-start py-4 px-5">
-            <span v-if="user.role === 0" class="inline-flex px-2 text-sm text-red-500 bg-red-100 rounded-2xl">
-              Admin
-            </span>
-            <span v-else class="inline-flex px-2 text-sm text-green-800 bg-green-100 rounded-2xl">
-              User
-            </span>
-          </div>
-          <div class="table-cell-string">
-            <v-mdi class="cursor-pointer" name="mdi-delete-sweep" fill="#423E41" @click="deleteUser(i)" />
-          </div>
-        </template>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import dayjs from 'dayjs'
-import { defineComponent, ref } from 'vue'
-import { useRouter } from 'vue-router'
+import ky from 'ky'
+import { defineComponent, onMounted, Ref, ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 
-import { users } from '@/constants/devtools-users'
+import { AdminGetUserDataOptions, AdminGetUserDataReply } from '@reeba/common'
+
+import { adminGetUserData, getUserAvatar } from '@/api/endpoints'
+import { useAuthStore } from '@/store/use-auth-store'
+import { formatQueryString, formatTimeString } from '@/utils'
 
 export default defineComponent({
   name: 'devtool-users',
   setup () {
     const router = useRouter()
+    const route = useRoute()
+    const authStore = useAuthStore()
 
-    const userRef = ref(users)
+    const sortOptions: Ref<AdminGetUserDataOptions> = ref('name-asc')
+    const userData: Ref<AdminGetUserDataReply> = ref({ total: 0, users: [] })
+    const page = ref(1)
 
-    const formatBirthdate = (birthdate: string): string => {
-      return dayjs(birthdate).format('MMMM D, YYYY')
-    }
+    watch(sortOptions, async () => {
+      await getAdminUsers()
+    })
 
-    const deleteUser = (index: number): void => {
-      if (userRef.value[index].role === 0) {
-        return
+    const getAdminUsers = async (): Promise<void> => {
+      const formattedPage = Number(formatQueryString(route.query.page, '1'))
+      page.value = formattedPage
+
+      try {
+        const { method, url } = adminGetUserData
+
+        const response = await ky(url, {
+          method,
+          headers: {
+            Authorization: `Bearer ${authStore.userData.token}`
+          },
+          searchParams: [
+            ['page', page.value],
+            ['sort', sortOptions.value]
+          ]
+        }).json<AdminGetUserDataReply>()
+
+        userData.value.total = response.total
+        userData.value.users = response.users
+      } catch (error) {
+        // @ts-expect-error error is unknown
+        const resp = error?.response
+
+        if (resp.status == null) {
+          router.push({ name: 'Not Found', params: { pathMatch: route.path.substring(1).split('/') }, query: route.query, hash: route.hash })
+        }
+
+        if (resp.status === 401) {
+          router.push({ name: 'Signin' })
+        }
+
+        if (resp.status === 403) {
+          router.push({ name: 'Home' })
+        }
+
+        router.push({ name: 'Not Found', params: { pathMatch: route.path.substring(1).split('/') }, query: route.query, hash: route.hash })
       }
-
-      userRef.value.splice(index, 1)
     }
 
-    const goToUsersPage = (index: number): void => {
-      router.push('/users')
-      console.log(index)
-    }
+    onMounted(async () => {
+      await getAdminUsers()
+    })
 
     return {
-      userRef,
-      formatBirthdate,
-      deleteUser,
-      goToUsersPage
+      page,
+      getUserAvatar,
+      userData,
+      sortOptions,
+      formatTimeString
     }
   }
 })
