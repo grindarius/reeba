91ba59d33d1b5a0d45fed1087f6e5e7669387b4e
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-04-04 02:31:59 +0700

    fallback to simple query and math function

--- packages/backend/src/routes/admin/get-admin-summary.ts
+++ packages/backend/src/routes/admin/get-admin-summary.ts
@@ -16,6 +16,27 @@ const schema: FastifySchema = {
   }
 }
 
+type DateInput = [Date, Date]
+
+/**
+ * Percentage difference calculator for user amount
+ *
+ * @see https://www.skillsyouneed.com/num/percent-change.html
+ */
+const percentageDifference = (now: number, then: number): number => {
+  if (then === 0) {
+    if (now - then > 0) {
+      return 100
+    }
+
+    if (now - then < 0) {
+      return -100
+    }
+  }
+
+  return (now - then) / then * 100
+}
+
 export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promise<void> => {
   instance.get<{ Querystring: AdminGetStatisticsSummaryRequestQuerystring, Reply: AdminGetStatisticsSummaryReply }>(
     '/summary',
@@ -31,91 +52,63 @@ export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promi
         }
       ]
     },
-    async () => {
-      const now = dayjs().toDate()
-      const twoMonthPrior = dayjs().subtract(2, 'month').startOf('month').toDate()
+    async (request) => {
+      const { start, end } = request.query
 
-      const newUsersGroupByMonths = await instance.pg.query<{ total_users_each_month: number, date: string }>(
-        `select
-          count(user_username) as total_users,
-          to_char(date_trunc($3, user_registration_datetime), $4)
-        from "users"
-        where $1 <= user_registration_datetime and user_registration_datetime <= $2
-        group by to_char(date_trunc($3, user_registration_datetime), $4)
-        order by to_char(date_trunc($3, user_registration_datetime), $4) asc`,
-        [twoMonthPrior, now, 'month', 'YYYY-MM-DD']
+      const now = dayjs().toDate()
+      const startOfNow = dayjs().startOf('month').toDate()
+      const oneMonthPrior = dayjs().subtract(1, 'month').startOf('month').toDate()
+
+      const totalUsers = await instance.pg.query<{ total_users: number }>(
+        'select count(user_username) as total_users from "users"'
       )
 
-      console.log(newUsersGroupByMonths.rows)
+      const newUsersThisMonth = await instance.pg.query<{ new_users_this_month: number }, DateInput>(
+        'select count(user_username) as new_users_this_month from "users" where $1 <= user_registration_datetime and user_registration_datetime <= $2',
+        [startOfNow, now]
+      )
 
-      // const totalUsers = await instance.pg.query<{ total_users: number }>(
-      //   'select count(user_username) as total_users from "users"'
-      // )
+      const newUsersPastMonth = await instance.pg.query<{ new_users_past_month: number }, DateInput>(
+        'select count(user_username) as new_users_past_month from "users" where $1 <= user_registration_datetime and user_registration_datetime <= $2',
+        [oneMonthPrior, startOfNow]
+      )
 
-      // const newUsersPast3Months = await instance.pg.query(
-      //   'select count(event_id) as event_count, date_trunc($1, event_creation_date) as event_date from "events" group by event_date',
-      //   ['month']
-      // )
+      const newUsersInTimeRange = await instance.pg.query<{ new_users_in_time_range: number }, DateInput>(
+        'select count(user_username) as new_users_in_time_range from "users" where $1 <= user_registration_datetime and user_registration_datetime <= $2',
+        [dayjs(start).toDate(), dayjs(end).toDate()]
+      )
 
-      // console.log(newUsersPast3Months.rows)
+      const totalEvents = await instance.pg.query<{ total_events: number }>(
+        'select count(events.event_id) as total_events from "events"'
+      )
 
-      // const newUsersThisMonth = await instance.pg.query<{ new_users_this_month: number }, DateInput>(
-      //   'select count(user_username) as new_users_this_month from "users" where user_registration_datetime <= $1 and $2 <= user_registration_datetime',
-      //   [oneMonthPrior, now]
-      // )
+      const newEventsThisMonth = await instance.pg.query<{ new_events_this_month: number }, DateInput>(
+        'select count(event_id) as new_events_this_month from "events" where $1 <= event_creation_date and event_creation_date <= $2',
+        [startOfNow, now]
+      )
 
-      // const newUsersPastMonth = await instance.pg.query<{ new_users_past_month: number }, DateInput>(
-      //   'select count(user_username) as new_users_past_month from "users" where user_registration_datetime <= $1 and $2 <= user_registration_datetime',
-      //   [twoMonthPrior, oneMonthPrior]
-      // )
+      const newEventsPastMonth = await instance.pg.query<{ new_events_past_month: number }>(
+        'select count(event_id) as new_events_past_month from "events" where $1 <= event_creation_date and event_creation_date <= $2',
+        [oneMonthPrior, startOfNow]
+      )
 
-      // const newUsersInTimeRange = await instance.pg.query<{ new_users_in_time_range: number }, DateInput>(
-      //   'select count(user_username) as new_users_in_time_range from "users" where user_registration_datetime <= $1 and $2 <= user_registration_datetime',
-      //   [dayjs(start).toDate(), dayjs(end).toDate()]
-      // )
-
-      // const totalEvents = await instance.pg.query<{ total_events: number }>(
-      //   'select count(events.event_id) as total_events from "events"'
-      // )
-
-      // const newEventsThisMonth = await instance.pg.query<{ new_events_this_month: number }, DateInput>(
-      //   'select count(event_id) as new_events_this_month from "events" where event_creation_date <= $1 and $2 <= event_creation_date',
-      //   [oneMonthPrior, now]
-      // )
-
-      // const newEventsPastMonth = await instance.pg.query<{ new_events_past_month: number }>(
-      //   'select count(event_id) as new_events_past_month from "events" where event_creation_date <= $1 and $2 <= event_creation_date',
-      //   [twoMonthPrior, oneMonthPrior]
-      // )
-
-      // const newEventsInTimeRange = await instance.pg.query<{ new_events_in_time_range: number }, DateInput>(
-      //   'select count(event_id) as new_events_in_time_range from "events" where event_creation_date <= $1 and $2 <= event_creation_date',
-      //   [dayjs(start).toDate(), dayjs(end).toDate()]
-      // )
+      const newEventsInTimeRange = await instance.pg.query<{ new_events_in_time_range: number }, DateInput>(
+        'select count(event_id) as new_events_in_time_range from "events" where $1 <= event_creation_date and event_creation_date <= $2',
+        [dayjs(start).toDate(), dayjs(end).toDate()]
+      )
 
       return {
-        totalUsers: 0,
-        newUsersThisMonth: 0,
-        newUsersPercentageDifferenceToLastMonth: 0,
-        newUsersInTimeRange: 0,
-        totalEvents: 0,
-        newEventsThisMonth: 0,
-        newEventsPercentageDifferenceToLastMonth: 0,
-        newEventsInTimeRange: 0
+        totalUsers: totalUsers.rows[0].total_users,
+        newUsersThisMonth: newUsersThisMonth.rows[0].new_users_this_month,
+        newUsersPastMonth: newUsersPastMonth.rows[0].new_users_past_month,
+        newUsersPercentageDifferenceToLastMonth: percentageDifference(newUsersThisMonth.rows[0].new_users_this_month, newUsersPastMonth.rows[0].new_users_past_month),
+        newUsersInTimeRange: newUsersInTimeRange.rows[0].new_users_in_time_range,
+        totalEvents: totalEvents.rows[0].total_events,
+        newEventsThisMonth: newEventsThisMonth.rows[0].new_events_this_month,
+        newEventsPastMonth: newEventsPastMonth.rows[0].new_events_past_month,
+        newEventsPercentageDifferenceToLastMonth: percentageDifference(newEventsThisMonth.rows[0].new_events_this_month, newEventsPastMonth.rows[0].new_events_past_month),
+        newEventsInTimeRange: newEventsInTimeRange.rows[0].new_events_in_time_range
       }
-
-      // return {
-      //   totalUsers: totalUsers.rows[0].total_users,
-      //   newUsersThisMonth: newUsersThisMonth.rows[0].new_users_this_month,
-      //   newUsersPercentageDifferenceToLastMonth: isNaN(((newUsersThisMonth.rows[0].new_users_this_month - newUsersPastMonth.rows[0].new_users_past_month) / newUsersPastMonth.rows[0].new_users_past_month) * 100),
-      //   newUsersInTimeRange: newUsersInTimeRange.rows[0].new_users_in_time_range,
-      //   totalEvents: totalEvents.rows[0].total_events,
-      //   newEventsThisMonth: newEventsThisMonth.rows[0].new_events_this_month,
-      //   newEventsPercentageDifferenceToLastMonth: isNaN(((newEventsThisMonth.rows[0].new_events_this_month - newEventsPastMonth.rows[0].new_events_past_month) / newEventsPastMonth.rows[0].new_events_past_month) * 100)
-      //     ? 0
-      //     : (((newEventsThisMonth.rows[0].new_events_this_month - newEventsPastMonth.rows[0].new_events_past_month) / newEventsPastMonth.rows[0].new_events_past_month) * 100),
-      //   newEventsInTimeRange: newEventsInTimeRange.rows[0].new_events_in_time_range
-      // }
     }
   )
 }
