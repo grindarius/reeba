8d29cdf78c5576f44215dc1b6386bc0842e4bb88
Baramee Chaiho <pahe.014700@gmail.com>
2022-04-06 03:37:06 +0700

    Created route get-follow

--- packages/backend/src/routes/followings/index.ts
+++ packages/backend/src/routes/followings/index.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify'
 
+import getFollowRoute from './get-follow'
 import postFollowRoute from './post-follow'
 
 export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promise<void> => {
   void instance.register(postFollowRoute)
+  void instance.register(getFollowRoute)
 }
