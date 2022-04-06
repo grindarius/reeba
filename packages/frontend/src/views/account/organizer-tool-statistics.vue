dafb58473ece07dc8b318932720630e529e42e20
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-04-07 03:20:16 +0700

    added api calls into statistics

--- packages/frontend/src/views/account/organizer-tool-statistics.vue
+++ packages/frontend/src/views/account/organizer-tool-statistics.vue
@@ -1,18 +1,244 @@
 <template>
-  <div class="organizer-tool-statistics-page">
-    <div class="organizer-tool-statistics-page-content">
-      <h1 class="text-4xl text-white">
-        organizer-tool-statistics
-      </h1>
+  <metainfo>
+    <template #title="{ content }">
+      {{ content }} | ReebA: Ticket booking. Redefined.
+    </template>
+  </metainfo>
+  <div class="container mx-auto">
+    <h1 class="text-4xl font-semibold text-white">
+      {{ overviewResponse?.name ?? '' }}
+    </h1>
+    <div class="text-lg breadcrumbs">
+      <ul>
+        <li>
+          <a>
+            Overview
+          </a>
+        </li>
+        <li>
+          <a>
+            Orders
+          </a>
+        </li>
+        <li>
+          <a>
+            Edit event
+          </a>
+        </li>
+      </ul>
     </div>
+    <div class="flex flex-col lg:flex-row gap-4 mt-6">
+      <div class="basis-1/5 rounded-lg shadow-md p-4 bg-base-200">
+        <h1 class="text-2xl font-bold">
+          Total tickets sold
+        </h1>
+        <div class="flex flex-row place-items-center justify-evenly">
+          <div class="radial-progress mt-5" :style="`--value: ${Math.round(overviewResponse?.seatFullnessPercentage ?? 0)};`">
+            {{ Math.round(overviewResponse?.seatFullnessPercentage ?? 0) }}%
+          </div>
+          <div class="text-xl">
+            {{ d3.format(',')(overviewResponse?.totalTakenSeats ?? 0) }} / {{ d3.format(',')(overviewResponse?.totalSeats ?? 0) }}
+          </div>
+        </div>
+      </div>
+      <div class="grow rounded-lg shadow-md p-4 bg-base-200">
+        <div class="text-2xl font-bold">
+          Sales summary
+        </div>
+        <div class="grid grid-cols-2 gap-y-3">
+          <h1 class="text-xl font-bold">
+            Gross ticket sales
+          </h1>
+          <h1 class="text-xl">
+            {{ d3.format(',')(overviewResponse?.grossTicketSales ?? 0) }} THB
+          </h1>
+          <div class="text-xl font-bold">
+            ReebA ticket fee
+          </div>
+          <h1 class="text-xl">
+            {{ d3.format(',')(overviewResponse?.reebaTicketFees ?? 0) }} THB
+          </h1>
+          <h1 class="text-xl font-bold">
+            Net payout
+          </h1>
+          <h1 class="text-xl">
+            {{ d3.format(',')(overviewResponse?.netPayout ?? 0) }} THB
+          </h1>
+        </div>
+      </div>
+    </div>
+    <h1 class="text-3xl font-bold my-4">
+      Users map
+    </h1>
+    <div id="organizer-world-map" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import * as d3 from 'd3'
+import { FeatureCollection, Geometry } from 'geojson'
+import * as i18nCountries from 'i18n-iso-countries'
+import en from 'i18n-iso-countries/langs/en.json'
+import ky from 'ky'
+import * as topojson from 'topojson-client'
+import { GeometryCollection, Topology } from 'topojson-specification'
+import { defineComponent, onMounted, Ref, ref } from 'vue'
+import { useMeta } from 'vue-meta'
+import { useRoute, useRouter } from 'vue-router'
+
+import {
+  GetOrganizerEventStatisticsReply,
+  GetOrganizerEventUsersMapReply
+} from '@reeba/common'
+
+import { getOrganizerMapOverview, getOrganizerOverviewData } from '@/api/endpoints'
+import countriesJson from '@/assets/world-topo.json'
+import { useAuthStore } from '@/store/use-auth-store'
+
+i18nCountries.registerLocale(en)
 
 export default defineComponent({
-  name: 'organizer-tool-statistics'
+  name: 'organizer-tool-statistics',
+  setup () {
+    const route = useRoute()
+    const router = useRouter()
+    const authStore = useAuthStore()
+
+    const width = 1000
+    const height = 500
+
+    const land = ref({}) as Ref<FeatureCollection<Geometry, { name: string }>>
+    const svg = ref() as Ref<d3.Selection<SVGSVGElement, unknown, HTMLElement, unknown>>
+    const projection = ref(d3.geoMercator().scale(100).center([0, 20]).translate([width / 2, height / 2]))
+    const path = ref(d3.geoPath().projection(projection.value))
+
+    const overviewResponse = ref<GetOrganizerEventStatisticsReply | undefined>(undefined)
+    const usersMapOverviewResponse = ref<GetOrganizerEventUsersMapReply>({ users: [] })
+
+    useMeta({
+      title: 'Organizer statistics'
+    })
+
+    const getOverviewData = async (): Promise<void> => {
+      try {
+        const { method, url } = getOrganizerOverviewData({ username: authStore.userData.username, eventId: route.params.eventId as string ?? '' })
+
+        const response = await ky(url, {
+          method,
+          headers: {
+            Authorization: `Bearer ${authStore.userData.token}`
+          }
+        }).json<GetOrganizerEventStatisticsReply>()
+
+        overviewResponse.value = response
+      } catch (error) {
+        // @ts-expect-error error is unknown
+        const resp = error?.response
+
+        if (resp.status == null) {
+          router.push({ name: 'Not Found', params: { pathMatch: route.path.substring(1).split('/') }, query: route.query, hash: route.hash })
+          return
+        }
+
+        if (resp.status === 401) {
+          router.push({ name: 'Signin' })
+          return
+        }
+
+        if (resp.status === 403) {
+          router.push({ name: 'Home' })
+          return
+        }
+
+        router.push({ name: 'Not Found', params: { pathMatch: route.path.substring(1).split('/') }, query: route.query, hash: route.hash })
+      }
+    }
+
+    const getUserMaps = async (): Promise<void> => {
+      try {
+        const { method, url } = getOrganizerMapOverview({ username: authStore.userData.username, eventId: route.params.eventId as string ?? '' })
+
+        const response = await ky(url, {
+          method,
+          headers: {
+            Authorization: `Bearer ${authStore.userData.token}`
+          }
+        }).json<GetOrganizerEventUsersMapReply>()
+
+        usersMapOverviewResponse.value = response
+      } catch (error) {
+        // @ts-expect-error error is unknown
+        const resp = error?.response
+
+        if (resp.status == null) {
+          router.push({ name: 'Not Found', params: { pathMatch: route.path.substring(1).split('/') }, query: route.query, hash: route.hash })
+          return
+        }
+
+        if (resp.status === 401) {
+          router.push({ name: 'Signin' })
+          return
+        }
+
+        if (resp.status === 403) {
+          router.push({ name: 'Home' })
+          return
+        }
+
+        router.push({ name: 'Not Found', params: { pathMatch: route.path.substring(1).split('/') }, query: route.query, hash: route.hash })
+      }
+    }
+
+    const createWorldMap = (): void => {
+      svg.value = d3.select('div#organizer-world-map')
+        .append('svg')
+        .attr('id', 'organizer-world-map-svg')
+        .attr('viewBox', `0 0 ${width} ${height}`)
+
+      // @ts-expect-error from how json calculates their type
+      land.value = topojson.feature(countriesJson as Topology, countriesJson.objects.countries as GeometryCollection<{ name: string }>)
+      updateWorldMap()
+    }
+
+    const updateWorldMap = (): void => {
+      svg.value.selectAll('path.world-map-path')
+        .data(land.value.features)
+        .join('path')
+        .attr('d', path.value)
+        .attr('class', 'world-map-path')
+        .attr('stroke', '#ddd')
+        .attr('stroke-width', '0.5px')
+        .attr('fill', (d) => {
+          const alpha2 = i18nCountries.numericToAlpha2(d.id ?? '')
+
+          if (alpha2 == null || alpha2 === '') {
+            return '#222'
+          }
+
+          const countriesToMap = usersMapOverviewResponse.value.users.find(u => u.country === alpha2)
+          if (countriesToMap == null) {
+            return '#222'
+          }
+
+          return '#d5a755'
+        })
+    }
+
+    onMounted(async () => {
+      await Promise.all([
+        getOverviewData(),
+        getUserMaps()
+      ])
+      createWorldMap()
+    })
+
+    return {
+      createWorldMap,
+      d3,
+      getUserMaps,
+      overviewResponse
+    }
+  }
 })
 </script>
 
