bb241af189c66ec60ec824b6f5f4a49950e5433b
Baramee Chaiho <pahe.014700@gmail.com>
2022-04-07 04:21:38 +0700

    Created  async request table user_followings

--- packages/backend/src/routes/users/get-user-following.ts
+++ packages/backend/src/routes/users/get-user-following.ts
@@ -4,25 +4,56 @@ import {
   GetUserFollowingsListReply,
   GetUserFollowingsListReplySchema,
   GetUserFollowingsListRequestParams,
-  GetUserFollowingsListRequestParamsSchema
+  GetUserFollowingsListRequestParamsSchema,
+  GetUserFollowingsListRequestQuertsring,
+  GetUserFollowingsListRequestQuertstringSchema
 } from '@reeba/common'
 
 const schema: FastifySchema = {
   params: GetUserFollowingsListRequestParamsSchema,
+  querystring: GetUserFollowingsListRequestQuertstringSchema,
   response: {
     200: GetUserFollowingsListReplySchema
   }
 }
 
 export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promise<void> => {
-  instance.get<{ Params: GetUserFollowingsListRequestParams, Reply: GetUserFollowingsListReply }>(
+  instance.get<{ Params: GetUserFollowingsListRequestParams, Querystring: GetUserFollowingsListRequestQuertsring, Reply: GetUserFollowingsListReply }>(
     '/:username/followings',
     {
       schema,
       onRequest: instance.authenticate,
-      preValidation: async () => {
+      preValidation: async (request, reply) => {
+        const { username } = request.params
+        const { u } = request.query
 
+        if (username == null || username === '') {
+          void reply.code(400)
+          throw new Error('params should have required property \'username\'')
+        }
+
+        if (u == null) {
+          request.query.u = ''
+        }
       }
-    }, async () => { }
+    },
+    async (request) => {
+      const { username } = request.params
+
+      const followings = await instance.pg.query<{ username: string, isAdmin: boolean, isVerified: boolean }>(
+        `select
+          user_followings.follower_username as username,
+          case when users.user_role = 'user' then 'true'::boolean else 'false'::boolean end "isAdmin",
+          users.user_verification_status as "isVerified"
+        from "user_followings"
+        inner join "users" on user_followings.follower_username = users.user_username
+        where user_followings.followed_username = $1`,
+        [username]
+      )
+
+      return {
+        followings: followings.rows
+      }
+    }
   )
 }
