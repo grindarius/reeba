15e5514ef4abb3156a20a8dd137f2cacfb060d9c
LzzAa35 <62424250+LzzAa35@users.noreply.github.com>
2022-04-07 15:34:23 +0700

    Chang line

--- packages/backend/src/routes/users/index.ts
+++ packages/backend/src/routes/users/index.ts
@@ -11,9 +11,9 @@ import removeUserRoute from './remove-user'
 export default async (instance: FastifyInstance, _: FastifyPluginOptions): Promise<void> => {
   void instance.register(getUserRoute)
   void instance.register(getUserFollowersRoute)
+  void instance.register(getFollowingRoute)
   void instance.register(getUserRelatedEventsRoute)
   void instance.register(manipulateAdminRoute)
   void instance.register(manipulateVerificationRoute)
   void instance.register(removeUserRoute)
-  void instance.register(getFollowingRoute)
 }
