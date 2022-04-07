e098244a23265bf74368b0d215705b6ab7f6f2af
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-04-07 14:25:28 +0700

    add metainfo to edit event

--- packages/frontend/src/views/account/organizer-tools/organizer-tool-edit-event.vue
+++ packages/frontend/src/views/account/organizer-tools/organizer-tool-edit-event.vue
@@ -1,17 +1,21 @@
 <template>
-  <div class="organizer-tool-edit-event-page">
-    <div class="organizer-tool-edit-event-page-content">
-      <h1 class="text-4xl font-bold text-white">
-        Edit event
-      </h1>
-    </div>
-  </div>
+  <metainfo>
+    <template #title="{ content }">
+      {{ content }} | ReebA: Ticket booking. Redefined.
+    </template>
+  </metainfo>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { useMeta } from 'vue-meta'
 
 export default defineComponent({
-  name: 'organizer-tool-edit-event'
+  name: 'organizer-tool-edit-event',
+  setup () {
+    useMeta({
+      title: 'Edit event'
+    })
+  }
 })
 </script>
