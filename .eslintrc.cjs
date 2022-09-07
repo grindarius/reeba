045f20bc0ffd2819ea9fb7c2efee27eb4842acba
Bhattarapong Somwong <numbbutt34685@gmail.com>
2022-09-07 22:09:32 +0700

    readjusting eslint rules

---
+++ .eslintrc.cjs
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-strongly-recommended',
+    'standard-with-typescript'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: [
+      './packages/*/tsconfig.json'
+    ]
+  },
+  plugins: [
+    'vue',
+    'simple-import-sort',
+    'unused-imports'
+  ],
+  rules: {
+    'no-void': ['error'],
+    'quotes': ['error', 'single', {
+      allowTemplateLiterals: true
+    }],
+    'quote-props': ['error', 'consistent'],
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [['^\\w'], ['^@\\w'], ['^', '^\\.'], ['^\\u0000']]
+      }
+    ],
+    'simple-import-sort/exports': ['error'],
+    'unused-imports/no-unused-imports': 'error',
+    'import/newline-after-import': ['error', { count: 1 }]
+  }
+}
