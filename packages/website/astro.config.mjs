915f740454e4d3ff823fb4446e166625b78091c4
Bhattarapong Somwong <numbbutt34685@gmail.com>
2025-07-28 04:33:36 +0700

    basic website changes

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -1,11 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
+import node from '@astrojs/node'
+import vue from '@astrojs/vue'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
-});
+    plugins: [tailwindcss()],
+  },
+
+  server: {
+    host: '127.0.0.1',
+  },
+
+  integrations: [vue()],
+  adapter: node({
+    mode: 'standalone',
+  }),
+})
+
---
+++ packages/website/src/components/Search.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { navigate } from 'astro:transitions/client'
+
+const searchTerm = ref('')
+
+const nagivateSearch = async () => {
+  const q = new URLSearchParams([['q', searchTerm.value]])
+  await navigate(`/search?${q.toString()}`)
+}
+</script>
+
+<template>
+  <input
+    type="text"
+    v-model="searchTerm"
+    placeholder="Search"
+    class="lg:w-96 input input-ghost"
+    @keyup.enter="nagivateSearch"
+  />
+</template>
